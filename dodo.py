90ac0be59ff9f202aebbe261bc791c0b12f61f5e
junger <joerg.unger@bam.de>
2021-02-06 08:58:17 +0100

    add doc for forward model

--- dodo.py
+++ dodo.py
@@ -1,7 +1,7 @@
 import glob
 
 def task_website():
-    pages = glob.glob("doc/*.md")
+    pages = glob.glob("docs/*.rst")
     return {
         "file_dep": pages + ["conf.py", "index.rst"],
         "actions": ["sphinx-build . .build"],
