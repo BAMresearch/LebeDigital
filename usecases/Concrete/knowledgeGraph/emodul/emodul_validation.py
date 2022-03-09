0af22085a11a95e6ad7e1125884f0d24b1338974
Horst Fellenberg <40174618+horstf@users.noreply.github.com>
2022-03-09 17:30:48 +0100

    add base code for assertion testing of violated shapes

--- usecases/Concrete/knowledgeGraph/emodul/emodul_validation.py
+++ usecases/Concrete/knowledgeGraph/emodul/emodul_validation.py
@@ -1,3 +1,4 @@
+from operator import imod
 import subprocess
 from time import sleep
 from turtle import pos
@@ -6,6 +7,8 @@ import os
 import sys
 from pathlib import Path
 from git import Repo
+from rdflib import Graph, URIRef
+from rdflib.namespace import SH
 
 baseDir0 = Path(__file__).resolve().parents[0]
 baseDir1 = Path(__file__).resolve().parents[1]
@@ -17,47 +20,58 @@ graphPath = os.path.join(baseDir0,'E-modul-processed-data/EM_Graph.ttl')
 processedDataPath = os.path.join(baseDir0,'E-modul-processed-data')
 
 """
+Given a path to a shacl shape and a path to an rdf file, this function tests the rdf data against the specified shacl shapes.
+The result is an rdflib graph containing the validation report, if it is empty the validation was successful.
+"""
+def test_file(shapepath: str, filepath: str) -> Graph:
+    with open(shapepath, 'r') as f:
+        shacl_shape = f.read()
+    with open(filepath, 'r') as f:
+        rdf_data = f.read()
+
+    # post both files to the validation endpoint of the docker app
+    post_data = {'rdf': rdf_data, 'shaclShape': shacl_shape}
+    r = s.post('http://localhost:8080/rdfconv/validate', files=post_data)
+    g = Graph()
+
+    # only parse graph if it contains any violations
+    if not r.text.startswith('VALID'):
+        g.parse(data=r.text, format='ttl')
+    
+    return g
+
+"""
+Returns true if the given shape is violated in the report.
+"""
+def violates_shape(validation_report: Graph, shape: URIRef):
+    validation_report.subjects(SH.sourceShape, None)
+    # check if the generator contains anything
+
+"""
 Clone and run the RDFConverter microservice through docker. This allows access to the schacl validation api via POST request.
 """
 Repo.clone_from('https://github.com/Mat-O-Lab/RDFConverter.git', os.path.join(baseDir1, 'RDFConverter'))
 subprocess.run(['docker-compose', '-f', os.path.join(baseDir1, 'RDFConverter/docker-compose.yml'), 'up', '-d', 'app'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
-passed = [] #list of filenames of files that passed validation
-failed = [] #list of tuples (filenames, reports) of files that failed validaton
-
 # read shacl shape
-with open(os.path.join(baseDir1, 'shape.ttl'), 'r') as f:
-    shacl_shape = f.read()
+shacl_path = os.path.join(baseDir1, 'shape.ttl')
 
 # sleep shortly to avoid access of the validation service before it is ready
 sleep(3)
 s = requests.session()
 
-"""
-Iterate over all rdf files in the processedDataPath. For each file f, we call the shacl validation service with our predefined shacl shape and validate this shape against f. This is done via HTTP POST request to the microservice.
-"""
-for rdf_file in (os.path.join(processedDataPath, f) for f in os.listdir(processedDataPath) if os.fsdecode(f).endswith('.ttl')):
-    with open(rdf_file, 'r') as f:
-        post_data = {'rdf': f.read(), 'shaclShape': shacl_shape}
-    # post files and obtain result of validation
-    r = s.post('http://localhost:8080/rdfconv/validate', files=post_data)
-    # check if the validation was successful or not
-    if r.text.startswith('VALID'):
-        passed.append(rdf_file)
-    else:
-        failed.append((rdf_file, r.text))
+# test the EM_Graph.ttl graph that was created in the steps before against the shacl shape
+rdf_file = os.path.join(processedDataPath, 'EM_Graph.ttl')
+g = test_file(shacl_path, rdf_file)
 
 # close session object and shutdown docker container
 s.close()
 subprocess.run(['docker-compose', '-f', os.path.join(baseDir1, 'RDFConverter/docker-compose.yml'), 'down'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
-# print passed files
-print('files that passed shacl validation:')
-for t in enumerate(passed, start=1):
-    print(f'{t[0]}. {t[1]}')
 
-# print failed files
-print('files that failed shacl validation:')
-for t in enumerate(failed, start=1):
-    print(f'{t[0]}. {t[1][0]}\n{t[1][1]}')
+# Tests specific assertions about the shacl validation report.
+# assert that certain violations did not occur:
+assert not violates_shape(g, None)
 
+# assert that certain violations occurred:
+# assert violates_shape(g, None)
