289a00024583d41ff776aba71b75dc10d8ff7bca
vandungdo <van-dung.do@bam.de>
2022-04-19 12:35:36 +0200

    test query script

--- usecases/Concrete/knowledgeGraph/emodul/emodul_test.py
+++ usecases/Concrete/knowledgeGraph/emodul/emodul_test.py
@@ -5,7 +5,9 @@ print('emodul test query for demo')
 print('input: name of the emodul expriment')
 print('output: processed data of that experiment and the specimen parameters')
 nameOfExperiment = 'Werner 7.0 M IV E-Modul 28d v. 06.08.14 Probe 6'
+print(input_emodul_data_for_calibration(nameOfExperiment)['processedDataPath'])
 df = pd.read_csv(input_emodul_data_for_calibration(nameOfExperiment)['processedDataPath'])
+
 
 print(df.head())
 print('specimen mass: ', input_emodul_data_for_calibration(nameOfExperiment)['specimenMass'])
