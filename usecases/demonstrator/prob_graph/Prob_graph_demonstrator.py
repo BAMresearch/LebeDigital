126e1e32be989af0bc8b481fb3d63c69e1b866bf
Jörg F. Unger <joergfunger@bam.de>
2022-06-28 06:50:59 +0200

    setup the basic environment to create graphs using graphviz

---
+++ usecases/demonstrator/prob_graph/Prob_graph_demonstrator.py
@@ -0,0 +1,14 @@
+import graphviz
+dot = graphviz.Digraph('round-table', comment='The Round Table')
+
+dot.node('A', 'King Arthur')
+
+dot.node('B', 'Sir Bedevere the Wise')
+
+dot.node('L', 'Sir Lancelot the Brave')
+
+dot.edges(['AB', 'AL'])
+
+dot.edge('B', 'L', constraint='false')
+
+dot.render(directory='doctest-output', view=True)
