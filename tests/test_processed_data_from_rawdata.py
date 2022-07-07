a9a423629d93296414f679e62910dc51106bdddb
Atul Agrawal <atulagrawal0711@gmail.com>
2022-07-07 14:46:10 +0200

    updated test file

--- tests/test_processed_data_from_rawdata.py
+++ tests/test_processed_data_from_rawdata.py
@@ -10,9 +10,9 @@ def test_emodul_generate_processed_data():
     Testing the processed data generation on a single experiment
     """
 
-    rawdata_location = '../../usecases/MinimumWorkingExample/Data/E-modul/BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4'
+    rawdata_location = '../usecases/MinimumWorkingExample/Data/E-modul/BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4'
     processeddat_location = './BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4' + '.csv'
 
     processed_data_from_rawdata(locationOfRawData=rawdata_location, locationOfProcessedData=processeddat_location)
 
-    assert os.path.exists('./BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4.csv') ==True
+    assert os.path.exists('./BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4.csv') == True
