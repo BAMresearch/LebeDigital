58520cfd71cd24f92c61f378ad84f6231818fcf9
eriktamsen <erik.tamsen@web.de>
2022-08-30 12:43:08 +0200

    extract data from KG for calibration

--- lebedigital/calibration/test_KG.py
+++ lebedigital/calibration/test_KG.py
@@ -1,4 +1,47 @@
 import rdflib
+import pandas as pd
+import numpy as np
+import fenics_concrete
+
+def cylinder_simulation(parameters):
+        """running the cylinder simulation
+        Optimized for a linear problem as we only need to compute once
+
+        Parameter
+        ---------
+        parameters: dic
+                dictionary with simulation parameters
+
+        Return
+        ------
+        slope: float
+                slope of the linear load displacement function
+                """
+
+        parameters['nu'] = 0.2
+        parameters['dim'] = 3
+
+        parameters['mesh_density'] = 10
+        parameters['log_level'] = 'WARNING'
+        parameters['bc_setting'] = 'fixed'
+
+        test_load = -0.05
+
+        experiment = fenics_concrete.ConcreteCylinderExperiment(parameters)
+
+        problem = fenics_concrete.LinearElasticity(experiment, parameters)
+        sensor = fenics_concrete.sensors.ReactionForceSensorBottom()
+
+        problem.add_sensor(sensor)
+        problem.experiment.apply_displ_load(test_load)
+        problem.solve()  # solving this
+        measured_force = problem.sensors[sensor.name].data[-1]
+
+        # compute slope of linear problem
+        slope = measured_force/test_load
+
+        return slope
+
 def query_objects(queries, graph):
         # function to get objects from specific subject, predicate pairs, knowing there is only one result
         results = {}
@@ -24,7 +67,7 @@ queries = {
         'file_name' : {'subject' : 'ns1:informationbearingentity8', 'predicate' : 'ns9:has_text_value' }
 }
 
-# Path to KG
+# Path to KG, for testing
 path_to_KG = '../../usecases/MinimumWorkingExample/emodul/knowledge_graphs/BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4.tll'
 
 # initialize the graph
@@ -34,12 +77,55 @@ knowledge_graph.parse(path_to_KG, format='ttl')
 # queries
 results = query_objects(queries,knowledge_graph)
 
+print('STEP 1 : getting data from knowledge graph')
 # geometry
-length = results['length']
+length = float(results['length'])
 print('Length: ', length)
-diameter = results['diameter']
+diameter = float(results['diameter'])
 print('Diameter: ',diameter)
 
 # experimental data
 file_path = results['path'] + '/' + results['file_name']
-print(file_path)
+print('File path: ',file_path)
+
+print('STEP 2 : reading processed data')
+# read csv into lists
+df = pd.read_csv(file_path)
+
+# trying to get rid of stupid values, i.e. positive displacements and forces
+# TODO: is there anything else ot "clean" the raw data?
+df = df[df['Transducer 1[mm]'] < 0]
+df = df[df['Transducer 2[mm]'] < 0]
+df = df[df['Transducer 3[mm]'] < 0]
+df = df[df['Force [kN]'] < 0]
+
+force_list = df['Force [kN]'].to_list()
+displacement_list_1 = np.array(df['Transducer 1[mm]'].to_list())
+displacement_list_2 = np.array(df['Transducer 2[mm]'].to_list())
+displacement_list_3 = np.array(df['Transducer 3[mm]'].to_list())
+
+print('STEP 3 : run a simulation')
+# testing the cylinder simulation
+parameters = fenics_concrete.Parameters()  # using the current default values
+
+parameters['radius'] = diameter/2  # in mm
+parameters['height'] = length   # in mm
+parameters['E'] = 100  # in kN/mm^2
+print(' - Youngs modulus: ', parameters['E'])
+
+simulation_slope = cylinder_simulation(parameters)
+
+print('Slope:', simulation_slope)
+
+force_list_1 = displacement_list_1*simulation_slope
+force_list_2 = displacement_list_2*simulation_slope
+force_list_3 = displacement_list_3*simulation_slope
+
+print('STEP 4 : TODO optimization')
+
+
+
+
+
+
+
