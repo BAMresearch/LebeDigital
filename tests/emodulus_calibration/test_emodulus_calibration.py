7f958287720b6cf3958a6aceec517fbf90e02d12
Atul Agrawal <atulagrawal0711@gmail.com>
2022-09-14 18:29:49 +0200

    even more boring changes

--- tests/emodulus_calibration/test_emodulus_calibration.py
+++ tests/emodulus_calibration/test_emodulus_calibration.py
@@ -8,7 +8,9 @@ from pathlib import Path
 def test_emodulus_calibration():
     ParentDir = os.path.dirname(os.path.dirname(os.path.dirname(Path(__file__))))
     path_KG = ParentDir + "/tests/emodulus_calibration/Wolf 8.2 Probe 1.csv"  # This is not used so doesnt matter.
-    path_calibrated = ParentDir + "/usecases/MinimumWorkingExample/emodul/calibrated_data"
+    path_calibrated = ParentDir + "/tests/emodulus_calibration"
+    print("Calibration test started")
     # TODO: add a seed in the calibration
     E_calibrated = perform_calibration(path_to_KG=path_KG,calibrated_data_path=path_calibrated,mode='cheap',KnowledgeGraph=False)
     assert np.mean(E_calibrated) == pytest.approx(95E03,rel = 50e03) # Just adding test for calibration, not for prediction
+    print("Calibration test completed")
