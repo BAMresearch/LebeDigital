5becbb9b16eb638f2e909c27c4854ca38d833aee
ckujath <cezary.kujath@bam.de>
2022-10-07 11:51:49 +0200

    creating samples/properties in main of expstep

--- usecases/MinimumWorkingExample/dodo.py
+++ usecases/MinimumWorkingExample/dodo.py
@@ -10,7 +10,7 @@ from lebedigital.mapping.emodul_mapping import generate_knowledge_graph
 
 from lebedigital.raw_data_processing.youngs_modulus_data \
     .emodul_generate_processed_data import processed_data_from_rawdata
-    
+
 from lebedigital.openbis.expstep import ExpStep
 
 from doit import get_var
@@ -22,15 +22,21 @@ config = {"mode": get_var('mode', 'cheap')}
 
 DOIT_CONFIG = {'verbosity': 2}
 
-#parent directory of the minimum working example
+# parent directory of the minimum working example
 ParentDir = os.path.dirname(Path(__file__))
 
 # defining paths
-emodul_output_directory = Path(ParentDir, 'emodul')  # folder with metadata yaml files
-raw_data_emodulus_directory = Path(ParentDir, 'Data', 'E-modul')  # folder with folders of raw data files
-metadata_emodulus_directory = Path(emodul_output_directory, 'metadata_yaml_files')  # folder with metadata yaml files
-processed_data_emodulus_directory = Path(emodul_output_directory, 'processed_data')  # folder with csv data files
-knowledge_graphs_directory = Path(emodul_output_directory, 'knowledge_graphs')  # folder with KG ttl files
+# folder with metadata yaml files
+emodul_output_directory = Path(ParentDir, 'emodul')
+# folder with folders of raw data files
+raw_data_emodulus_directory = Path(ParentDir, 'Data', 'E-modul')
+# folder with metadata yaml files
+metadata_emodulus_directory = Path(
+    emodul_output_directory, 'metadata_yaml_files')
+processed_data_emodulus_directory = Path(
+    emodul_output_directory, 'processed_data')  # folder with csv data files
+knowledge_graphs_directory = Path(
+    emodul_output_directory, 'knowledge_graphs')  # folder with KG ttl files
 
 # when "cheap option" is run, only this souce of raw data is processed
 cheap_example_name = 'Wolf 8.2 Probe 1'
@@ -38,22 +44,27 @@ cheap_example_name = 'Wolf 8.2 Probe 1'
 # create folder, if it is not there
 Path(emodul_output_directory).mkdir(parents=True, exist_ok=True)
 
-#extract standardized meta data for Young' modulus tests
+# extract standardized meta data for Young' modulus tests
+
+
 def task_extract_metadata_emodul():
     # create folder, if it is not there
     Path(metadata_emodulus_directory).mkdir(parents=True, exist_ok=True)
 
     # setting for fast test, defining the list
     if config['mode'] == 'cheap':
-        list_raw_data_emodulus_directories = [ Path(raw_data_emodulus_directory, cheap_example_name) ]
-    else: # go through all files
-        list_raw_data_emodulus_directories = os.scandir(raw_data_emodulus_directory)
+        list_raw_data_emodulus_directories = [
+            Path(raw_data_emodulus_directory, cheap_example_name)]
+    else:  # go through all files
+        list_raw_data_emodulus_directories = os.scandir(
+            raw_data_emodulus_directory)
 
     for f in list_raw_data_emodulus_directories:
         if f.is_dir():
             raw_data_path = Path(f)
             raw_data_file = Path(f, 'specimen.dat')
-            yaml_metadata_file = Path(metadata_emodulus_directory, f.name + '.yaml')
+            yaml_metadata_file = Path(
+                metadata_emodulus_directory, f.name + '.yaml')
             yield {
                 'name': yaml_metadata_file,
                 'actions': [(emodul_metadata, [raw_data_path, yaml_metadata_file])],
@@ -62,23 +73,28 @@ def task_extract_metadata_emodul():
                 'clean': [clean_targets]
             }
 
-#extract standardized processed data for Young' modulus tests
+# extract standardized processed data for Young' modulus tests
+
+
 def task_extract_processed_data_emodul():
     # create folder, if it is not there
     Path(processed_data_emodulus_directory).mkdir(parents=True, exist_ok=True)
 
     # setting for fast test, defining the list
     if config['mode'] == 'cheap':
-        list_raw_data_emodulus_directories = [ Path(raw_data_emodulus_directory, cheap_example_name) ]
-    else: # go through all files
-        list_raw_data_emodulus_directories = os.scandir(raw_data_emodulus_directory)
+        list_raw_data_emodulus_directories = [
+            Path(raw_data_emodulus_directory, cheap_example_name)]
+    else:  # go through all files
+        list_raw_data_emodulus_directories = os.scandir(
+            raw_data_emodulus_directory)
 
     for f in list_raw_data_emodulus_directories:
         if f.is_dir():
             raw_data_file = Path(f, 'specimen.dat')
-            #the name of the csv file is the file name of the raw data
+            # the name of the csv file is the file name of the raw data
             # is processed_data_directory + directory_raw_data.csv
-            csv_data_file = Path(processed_data_emodulus_directory, f.name + '.csv')
+            csv_data_file = Path(
+                processed_data_emodulus_directory, f.name + '.csv')
 
             yield {
                 'name': csv_data_file,
@@ -89,7 +105,7 @@ def task_extract_processed_data_emodul():
             }
 
 
-#generate knowledgeGraphs
+# generate knowledgeGraphs
 @create_after(executed='extract_metadata_emodul')
 def task_export_knowledgeGraph_emodul():
     # create folder, if it is not there
@@ -97,8 +113,9 @@ def task_export_knowledgeGraph_emodul():
 
     # setting for fast test, defining the list
     if config['mode'] == 'cheap':
-        list_metadata_yaml_files = [ Path(metadata_emodulus_directory, cheap_example_name + '.yaml') ]
-    else: # go through all files
+        list_metadata_yaml_files = [
+            Path(metadata_emodulus_directory, cheap_example_name + '.yaml')]
+    else:  # go through all files
         # list of all meta data files....
         list_metadata_yaml_files = os.scandir(metadata_emodulus_directory)
 
@@ -111,25 +128,27 @@ def task_export_knowledgeGraph_emodul():
             name_of_ttl = f.name.replace('.yaml', '.ttl')
             name_of_cvs = f.name.replace('.yaml', '.csv')
             # path the processed data csv
-            processed_data_file_path = Path(processed_data_emodulus_directory, name_of_cvs)
+            processed_data_file_path = Path(
+                processed_data_emodulus_directory, name_of_cvs)
             # path to output file KG
-            knowledge_graph_file = Path(knowledge_graphs_directory, name_of_ttl)
+            knowledge_graph_file = Path(
+                knowledge_graphs_directory, name_of_ttl)
 
-            yield{
+            yield {
                 'name': knowledge_graph_file,
                 'actions': [(generate_knowledge_graph, [metadata_file_path,
-                                                    knowledge_graph_file])],
+                                                        knowledge_graph_file])],
                 'file_dep': [metadata_file_path, processed_data_file_path],
                 'targets': [knowledge_graph_file],
                 'clean': [clean_targets]
             }
-            
-@create_after(executed='export_knowledgeGraph_emodul')
-def task_upload_to_datastore():
-    
-    metadata_path = Path(metadata_emodulus_directory)
-    processed_data_path = Path(processed_data_emodulus_directory)
-    knowledge_graphs_path = Path(knowledge_graphs_directory)
-    
-    
-    print('done')
+
+# @create_after(executed='export_knowledgeGraph_emodul')
+# def task_upload_to_datastore():
+
+#     metadata_path = Path(metadata_emodulus_directory)
+#     processed_data_path = Path(processed_data_emodulus_directory)
+#     knowledge_graphs_path = Path(knowledge_graphs_directory)
+
+
+#     print('done')
