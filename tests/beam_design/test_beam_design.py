0d88ce75f0356390f4f5c6e37bd9a5fc19f6dbf0
raviapatel <ravee.a.patel@gmail.com>
2022-11-18 15:46:42 +0100

    Update test_beam_design.py
    removed extra function tests and prints. Added exact assert on value in test

--- tests/beam_design/test_beam_design.py
+++ tests/beam_design/test_beam_design.py
@@ -1,7 +1,7 @@
 import sys,os
 sys.path.append(os.path.join("..",".."))
 from lebedigital.BeamDesign import BeamDesign as bd
-
+import pytest
 def test_beam_design():
     """
     Test function to test beam design module 
@@ -9,21 +9,6 @@ def test_beam_design():
     https://www.structuralguide.com/worked-example-singly-reinforced-beam-design-using-ec2/
     """
     b,d = bd.section_dimension_rule_of_thumb(span=6750) #mm
-    MaxMoment,MaxShearForce =  bd.max_bending_moment_and_shear_force(span=6750,point_load=36e3,distributed_load=0)
-    out=  bd.beam_section_design(span =6750,
-                              b = b,
-                              d = d,
-                              MaxMoment=MaxMoment,
-                              MaxShearForce=MaxShearForce,
-                              fck=20,
-                              fyk=500,
-                              steelDia=12,
-                              cover=25)
-    for key,val in out.items():
-        print(f"{key} --> {val}")
-    assert(out["bottom_steel_numbers"]==3)
-    assert(out["shear_reinforcement_spacing[mm]"]==300)
-    #this is test on function check_design
     out=  bd.check_design(span =6750,
                               b = b,
                               d = d,
@@ -34,6 +19,6 @@ def test_beam_design():
                               steelDia=12,
                               n_bottom=3,
                               cover=25)
-    assert(out > 0)
+    assert(out == pytest.approx(40.7447380827))
 if __name__ =="__main__":
 	test_beam_design()
