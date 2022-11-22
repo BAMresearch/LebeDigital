c65cef0d1f58c7253d047a2a91c7f31f28f1ba94
raviapatel <ravee.a.patel@gmail.com>
2022-11-22 14:25:12 +0100

    ravi patel (#68)
    * ravi patel
    
    Added script for beam design
    
    * Update test_beam_design.py
    
    Fixed import issue
    
    * Create __init__.py
    
    __init__ file added to fix import issue
    
    * Update test_beam_design.py
    
    one more try to fix import error!!
    
    * Update BeamDesign.py
    
    udapted documentation as per the request
    
    * modification of test
    
    added assertion in test to compare with the result output
    
    * added function check design
    
    I have added function check _design which can be used for optimization. It gives difference between specified_area and required_area as output. I added also test for check_desgin in test_beam_design function in test script. I also keep old tests so that other functions are also tested
    
    * Update test_beam_design.py
    
    removed extra function tests and prints. Added exact assert on value in test
    
    * fixed some minor details
    
    Co-authored-by: Erik Tamsen <git@eriktamsen.com>

---
+++ tests/beam_design/test_beam_design.py
@@ -0,0 +1,28 @@
+from lebedigital.BeamDesign import beam_design
+import pytest
+
+
+def test_beam_design():
+    """
+    Test function to test beam design module 
+    This example is analogus to one described in this blog: 
+    https://www.structuralguide.com/worked-example-singly-reinforced-beam-design-using-ec2/
+    """
+
+    width, height = beam_design.section_dimension_rule_of_thumb(span=6750) # mm
+    out = beam_design.check_design(span=6750,
+                                   width=width,
+                                   height=height,
+                                   point_load = 36e3,
+                                   distributed_load= 0,
+                                   fck=20,
+                                   fyk=500,
+                                   steel_dia=12,
+                                   n_bottom=3,
+                                   cover=25)
+
+    assert(out == pytest.approx(0.13647667348210732))
+
+
+if __name__ == "__main__":
+    test_beam_design()
