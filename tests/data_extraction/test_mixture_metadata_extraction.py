6d17ad62b7708896ba1d1946dcc70eb2aa0c5bc2
soudehMasoudian <soudeh.masoudian@gmail.com>
2022-11-22 22:42:18 +0100

    fix problems in unittests after putting into separated folders

--- tests/data_extraction/test_mixture_metadata_extraction.py
+++ tests/data_extraction/test_mixture_metadata_extraction.py
@@ -1,55 +1,27 @@
-import pytest
-from lebedigital.raw_data_processing.metadata_extraction \
-    .mixture_metadata_extraction import extraction
+import os
+from lebedigital.raw_data_processing.mixture \
+    .mixture_metadata_extraction import extract_metadata_mixture
+from config import Config
+import unittest
 
 
-def test_extraction():
-    """Tesing the mixture metadata extraction on a single example"""
+class TestMixtureMetadataExtraction(unittest.TestCase):
+    def test_extraction(self):
+        """Testing the mixture metadata extraction on a single example"""
 
-    # setting up the test example
-    input = '../usecases/MinimumWorkingExample/Data/Mischungen/2014_08_05 Rezeptur_MIII.xlsx'
-    target_data = {'Cement -- Quantity in mix': 330, 'Cement -- Specific gravity': 3.123,
-                   'Cement -- Annotation': 'CEM I 42,5 R', 'Water (total) -- Quantity in mix':
-                    175, 'Water (total) -- Specific gravity': 1, 'Water (total) -- Annotation': 
-                    '---', 'Water (effective) -- Quantity in mix': 
-                    '---', 'Water (effective) -- Specific gravity': 
-                    '---', 'Water (effective) -- Annotation': '---', 'Air -- Quantity in mix': 
-                    '---', 'Air -- Specific gravity': '---', 'Air -- Annotation': '---', 
-                    'Supplementary cementious materials Kalksteinmehl -- Quantity in mix': 147, 
-                    'Supplementary cementious materials Kalksteinmehl -- Specific gravity': 2.74,
-                    'Supplementary cementious materials Kalksteinmehl -- Annotation': 
-                    'Medenbach', 'Admixture -- Quantity in mix': 4.95, 
-                    'Admixture -- Specific gravity': 1.05, 'Admixture -- Annotation': 
-                    'FM 595 BASF', 'Total -- Quantity in mix': 656.95, 
-                    'Total -- Specific gravity': '---', 'Total -- Annotation': '---', 
-                    'Zuschlag (total) -- Quantity in mix': 1685, 
-                    'Zuschlag (total) -- Specific gravity': '---', 
-                    'Zuschlag (total) -- Annotation': '---', '0 / 0,3 -- Quantity in mix': 0, 
-                    '0 / 0,3 -- Specific gravity': 2.65, '0 / 0,3 -- Annotation': 'Quarz', 
-                    '0,1 / 0,5 -- Quantity in mix': 337, '0,1 / 0,5 -- Specific gravity': 2.63, 
-                    '0,1 / 0,5 -- Annotation': 'Okrilla  ', '0,5 / 1,0 -- Quantity in mix': 194, 
-                    '0,5 / 1,0 -- Specific gravity': 2.63,
-                    '0,5 / 1,0 -- Annotation': 'Okrilla  ', '1,0 / 2,0 -- Quantity in mix': 194, 
-                    '1,0 / 2,0 -- Specific gravity': 2.63, '1,0 / 2,0 -- Annotation': 'Okrilla  ', 
-                    '2,0 / 4,0 -- Quantity in mix': 126, '2,0 / 4,0 -- Specific gravity': 2.63, 
-                    '2,0 / 4,0 -- Annotation': 'Okrilla  ', '4,0 / 8,0 -- Quantity in mix': 126, 
-                    '4,0 / 8,0 -- Specific gravity': 2.63, '4,0 / 8,0 -- Annotation': 'Okrilla  ', 
-                    '8,0 / 16,0 -- Quantity in mix': 708, '8,0 / 16,0 -- Specific gravity': 2.63, 
-                    '8,0 / 16,0 -- Annotation': 'Okrilla  ', 'nan -- Quantity in mix': '---', 
-                    'nan -- Specific gravity': '---', 'nan -- Annotation': '---', 
-                    'Fresh concrete -- Quantity in mix': 2341.95, 
-                    'Fresh concrete -- Specific gravity': '---', 'Fresh concrete -- Annotation': 
-                    '---', 'Mehlkornanteil -- Quantity in mix': '---', 
-                    'Mehlkornanteil -- Specific gravity': '---', 'Mehlkornanteil -- Annotation': 
-                    '---', 'Mörtelanteil -- Quantity in mix': '---', 
-                    'Mörtelanteil -- Specific gravity': '---', 'Mörtelanteil -- Annotation': '---', 
-                    'Date': '2014-06-30', 'Editor': 'Werner', 'Requester': 'BAM 7.0', 
-                    'Project number': 'BA-Losert', 'Specimen': 'BA-Losert MIII', 
-                    'Betonsorte u -festigkeitsklasse': '---', 'Wasserzementwert': '0,53 w/b = 0,37', 
-                    'Konsistenz': 'SVB', 'Sieblinie n. DIN 1045': 'SVB', 'Körnungsziffer': 3.88, 
-                    'Vol Leim/Zuschlag': 56.00624024960999}
-    # run extraction and getting a dictionary with metadata
-    test_data = extraction(input, None)
+        # setting up the test example input
+        input_path = '../usecases/MinimumWorkingExample/Data/Mischungen/2014_08_05 Rezeptur_MI.xlsx'
 
-    # checking if result is correct
-    assert test_data == target_data
+        target_data = Config.TEST_MIXTURE_METADATA_EXTRACTION_EXPECTED_VALUE
+        # run extraction and getting a dictionary with metadata
+        test_data = extract_metadata_mixture(input_path, None)
+
+        # make sure if the path exists
+        self.assertTrue(os.path.exists(input_path))
+
+        # checking if result is correct
+        self.assertEqual(test_data.__str__(), target_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/data_extraction/test_processed_data_from_rawdata.py
+++ tests/data_extraction/test_processed_data_from_rawdata.py
@@ -1,18 +1,20 @@
-import pytest
+import unittest
 import os
 
 from lebedigital.raw_data_processing.youngs_modulus_data.emodul_generate_processed_data import \
     processed_data_from_rawdata
 
+class TestProcessedDataFromRawData(unittest.TestCase):
 
-def test_emodul_generate_processed_data():
-    """
-    Testing the processed data generation on a single experiment
-    """
+    def test_emodul_generate_processed_data(self):
+        """
+        Testing the processed data generation on a single experiment
+        """
 
-    rawdata_location = '../usecases/MinimumWorkingExample/Data/E-modul/BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4'
-    processeddat_location = './BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4' + '.csv'
+        rawdata_location = '../usecases/MinimumWorkingExample/Data/E-modul/BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4'
+        processeddat_location = './BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4' + '.csv'
 
-    processed_data_from_rawdata(locationOfRawData=rawdata_location, locationOfProcessedData=processeddat_location)
+        processed_data_from_rawdata(locationOfRawData=rawdata_location, locationOfProcessedData=processeddat_location)
 
-    assert os.path.exists('./BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4.csv') == True
+        self.assertTrue(os.path.exists('./BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4.csv'))
+
