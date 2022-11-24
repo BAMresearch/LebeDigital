80054d8f64af96abac64ae12ddb373a52dea3c69
Erik Tamsen <git@eriktamsen.com>
2022-11-24 13:17:56 +0100

    working on including graph in workflow

--- usecases/optimization_paper/dodo.py
+++ usecases/optimization_paper/dodo.py
@@ -1,14 +1,43 @@
 import pathlib
-from doit import get_var
-from doit.action import CmdAction
-from doit.tools import config_changed
+from workflow_graph.paper_workflow_graph import build_workflow_graph
+from doit import create_after
 
+# testing stuff
+ROOT = pathlib.Path(__file__).parent
+
+# directories
+paper_dir = 'tex'
+figures_dir = 'figures'
+
+# paper
+paper_name = 'optimization_paper'
+
+# workflow graph
+workflow_graph_script = "paper_workflow_graph.py"
+workflow_graph_name = "optimization_paper_graph"
+workflow_graph_dir = "workflow_graph"
+workflow_output_name = figures_dir + '/' + workflow_graph_name
+workflow_output_file = ROOT / figures_dir  / (workflow_graph_name + '.gz.pdf')
+
+
+
+def task_build_graph():
+    return {
+        "file_dep": [workflow_graph_dir + '/' + workflow_graph_script],
+        "actions": [(build_workflow_graph,[workflow_output_name,False])],
+        "targets": [workflow_output_file],
+        "clean": True,
+    }
+
+
+@create_after(executed='build_graph')
 def task_paper():
     """compile pdf from latex source"""
-    paper = "tex/optimization_paper.tex"
+    paper_tex = paper_dir + '/' + paper_name + '.tex'
     return {
-        "file_dep": [paper],
-        "actions": [f"tectonic {paper}"],
-        "targets": ["optimization_paper.pdf"],
+        # TODO find out why this dependency throws an error
+        "file_dep": [paper_tex, workflow_output_file],
+        "actions": [f"tectonic {paper_tex}"],
+        "targets": [paper_name + '.pdf'],
         "clean": True,
     }
