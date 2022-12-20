369f3c74e222471fd921799a724ea0a042d70b9e
Erik Tamsen <git@eriktamsen.com>
2022-12-20 07:40:13 +0100

    better documentation of pint for function

--- lebedigital/demonstrator_scripts/youngs_modulus_approximation.py
+++ lebedigital/demonstrator_scripts/youngs_modulus_approximation.py
@@ -5,20 +5,24 @@ from lebedigital.unit_registry import ureg
 def youngs_modulus_approximation(fc,density) :
     """
     This is the function to approximate Young's modulus from compressive strength, according to ACI 363
+
+    The input and output is wrapped by the python pint package: https://pint.readthedocs.io/
+    This requires units to be attached to the input values.
+
     This source was chosen based on the paper:
     Sun, Fanourakis: The validation of elastic modulus models: Code models and their modified versions, 2021
     DOI: 10.1002/suco.202100312
 
     Parameters
     ----------
-    fc : float
+    fc : float / pint stress unit, will be converted to 'MPa'
         concrete compressive strength after 28 days
-    density: float
+    density: float / pint density unit, will be converted to 'kg/m^3'
         concrete density
 
     Returns
     -------
-    youngs_modulus : float
+    youngs_modulus : float / pint stress unit, will be in 'MPa'
         approximated youngs modulus
     """
 
