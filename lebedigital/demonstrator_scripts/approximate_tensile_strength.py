98c6ca71378ea54dbd9e43404b817e0c711b9ddc
Erik Tamsen <git@eriktamsen.com>
2022-12-20 08:12:06 +0100

    add documentation to tensile strength function

--- lebedigital/demonstrator_scripts/approximate_tensile_strength.py
+++ lebedigital/demonstrator_scripts/approximate_tensile_strength.py
@@ -5,14 +5,17 @@ def approximate_tensile_strength(compressive_strength):
     """
     function to approximate tensile strength from compressive strength
 
+    The input is checked to be using the python pint package: https://pint.readthedocs.io/
+    This requires the correct units to be attached to the input value.
+
     Parameters
     ----------
-    compressive_strength : float
+    compressive_strength : float / pint stress unit
         fc for concrete
 
     Returns
     -------
-    tensile_strength : float
+    tensile_strength : float / pint stress unit, the same unit as the input
         concrete compressive strength
     """
     tensile_strength = compressive_strength/10  # rough approximation
