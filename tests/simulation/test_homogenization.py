d579f1a72a84ce88242fd94afcd1497a926e42cc
Erik Tamsen <git@eriktamsen.com>
2022-12-21 12:11:04 +0100

    removed comments for units

--- tests/simulation/test_homogenization.py
+++ tests/simulation/test_homogenization.py
@@ -13,25 +13,24 @@ def test_homogenization():
     # values are kind of made up but within the expected magnitude
 
     # paste data
-    parameters['paste_E'] = 30e9 * ureg('Pa') # Pa
+    parameters['paste_E'] = 30e9 * ureg('Pa')
     parameters['paste_nu'] = 0.2
-    parameters['paste_C'] = 870 * ureg('J/kg') # J/kg Specific Heat Capacity
-    parameters['paste_kappa'] = 1.8 * ureg('W/m/K') # W/m/K Thermal conductivity
-    parameters['paste_rho'] = 2400 * ureg('kg/m^3') # kg/m^3
-    parameters['paste_fc'] = 30e6 * ureg('Pa') # Pa
-    parameters['paste_Q'] = 250000 * ureg('J/kg') # J/kg
+    parameters['paste_C'] = 870 * ureg('J/kg')  # Specific Heat Capacity
+    parameters['paste_kappa'] = 1.8 * ureg('W/m/K')  # Thermal conductivity
+    parameters['paste_rho'] = 2400 * ureg('kg/m^3')
+    parameters['paste_fc'] = 30e6 * ureg('Pa')
+    parameters['paste_Q'] = 250000 * ureg('J/kg')
 
     # aggregate data
-    parameters['aggregates_E'] = 25e9 * ureg('Pa')  # Pa
+    parameters['aggregates_E'] = 25e9 * ureg('Pa')
     parameters['aggregates_nu'] = 0.3
-    parameters['aggregates_C'] = 840 * ureg('J/kg') # J/kg Specific Heat Capacity
-    parameters['aggregates_kappa'] = 0.8 * ureg('W/m/K') # W/m/K Thermal conductivity
-    parameters['aggregates_rho'] = 2600 * ureg('kg/m^3') # kg/m^3
+    parameters['aggregates_C'] = 840 * ureg('J/kg')  # Specific Heat Capacity
+    parameters['aggregates_kappa'] = 0.8 * ureg('W/m/K')  # Thermal conductivity
+    parameters['aggregates_rho'] = 2600 * ureg('kg/m^3')
     parameters['aggregates_vol_frac'] = 0.6
 
     results = concrete_homogenization(parameters)
 
-    #assert results['E'] == pytest.approx(27014932516.511917* ureg('Pa') )
     assert_approx(results['E'], 27014932516.511917 * ureg('Pa'), rtol=0.001)
     assert results['nu'] == pytest.approx(0.26409495548961426)
     assert_approx(results['fc'], 27652173.91304348 * ureg('Pa'), rtol=0.001)
