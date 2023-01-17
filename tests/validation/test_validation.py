933d7830d50a08606671af5293beb80546298628
Horst Fellenberg <40174618+horstf@users.noreply.github.com>
2023-01-17 15:23:31 +0100

    fix wrong import in validation tests

--- tests/validation/test_validation.py
+++ tests/validation/test_validation.py
@@ -8,8 +8,8 @@ def test_graph_against_shape():
     Testing a data graph against a shapes graph and checking that the appropriate shapes fail.
     """
 
-    shapes_location = "validation/shape.ttl"
-    data_location = "validation/graph.ttl"
+    shapes_location = "./validation/shape.ttl"
+    data_location = "./validation/graph.ttl"
 
     g = read_graph_from_file(data_location)
     s = read_graph_from_file(shapes_location)
