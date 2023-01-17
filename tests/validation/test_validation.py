781d36e6adb077c861ea4d7f0dd8437229e8fc29
Horst Fellenberg <40174618+horstf@users.noreply.github.com>
2023-01-17 15:05:28 +0100

    fix wrong import in validation tests

--- tests/validation/test_validation.py
+++ tests/validation/test_validation.py
@@ -1,15 +1,15 @@
 import pytest
 import os
 
-from lebedigital.validation import SCHEMA, read_graph_from_file, test_graph as graph_test, violates_shape, graph_from_path_violates_shapes
+from lebedigital.validation import SCHEMA, read_graph_from_file, test_graph as graph_test, violates_shape
 
 def test_graph_against_shape():
     """
     Testing a data graph against a shapes graph and checking that the appropriate shapes fail.
     """
 
-    shapes_location = "../usecases/MinimumWorkingExample/emodul/validation_test/shape.ttl"
-    data_location = "../usecases/MinimumWorkingExample/emodul/validation_test/graph.ttl"
+    shapes_location = "validation/shape.ttl"
+    data_location = "validation/graph.ttl"
 
     g = read_graph_from_file(data_location)
     s = read_graph_from_file(shapes_location)
