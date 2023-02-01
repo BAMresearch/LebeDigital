26c924faf605d297577aae06bc05501d9fd8579c
Erik Tamsen <git@eriktamsen.com>
2023-02-01 11:31:55 +0100

    added GWP scripts

--- lebedigital/demonstrator_scripts/computation_GWP_mix.py
+++ lebedigital/demonstrator_scripts/computation_GWP_mix.py
@@ -3,26 +3,27 @@ from lebedigital.unit_registry import ureg
 
 def computation_GWP_mix(constituents):
     """
-    This function computes the global warming potential for a unit of beam
+    This function computes the global warming potential for a square meter of cement mix
 
     Parameters
     ----------
-    gwp_mix : float / pint unit in kg_CO2_eq/m^3
-        GWP per cubic meter of used mix
-    width : float / pint unit length
-        width of the beam
-    height : float / pint unit length
-        height of the beam
-    length : float / pint unit length
-        length of the beam
+    constituents : dict
+        the dictonary expects e.g.:
+        {'cement' : {'content': 5 * ureg('kg/m^3'), 'GWP': 2 * ureg('kg_CO2_eq/kg')},
+         'aggregates': {'content': 10 * ureg('kg/m^3'), 'GWP': 1 * ureg('kg_CO2_eq/kg')}}
 
     Returns
     -------
-    beam_gwp : float / pint unit, will be in 'kg_CO2_eq'
-       GWP of one beam
+    gwp_mix : float / pint unit, will be in 'kg_CO2_eq/m^3'
+       GWP of one cubic meter of concrete
     """
 
+    # initialize mix gwp, with unit
     gwp_mix = 0 * ureg('kg_CO2_eq/m^3')
+
+    # possibility to check if specific values are included
+    assert 'cement' in constituents.keys()
+
     for constituent in constituents.keys():
         # check and convert units
         constituents[constituent]['content'].ito('kg/m^3')
@@ -31,12 +32,12 @@ def computation_GWP_mix(constituents):
 
     return gwp_mix
 
+
 if __name__ == "__main__":
     # test while developing this
-    constituents = {'concrete' : {'content': 5 * ureg('kg/m^3'), 'GWP': 2 * ureg('kg_CO2_eq/kg') },
+    constituents = {'cement' : {'content': 5 * ureg('kg/m^3'), 'GWP': 2 * ureg('kg_CO2_eq/kg') },
                     'aggregates': {'content': 10 * ureg('kg/m^3'), 'GWP': 1 * ureg('kg_CO2_eq/kg') },
                     'slag': {'content': 5 * ureg('kg/m^3'), 'GWP': 20 * ureg('kg_CO2_eq/kg') }
                     }
-
 
     print(computation_GWP_mix(constituents))
