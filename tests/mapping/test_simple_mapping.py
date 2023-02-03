f52831939e40125e1f31841f5eb055abb49f1ccd
soudehMasoudian <soudeh.masoudian@gmail.com>
2023-02-03 16:48:09 +0100

    change the path to relative

--- tests/mapping/test_simple_mapping.py
+++ tests/mapping/test_simple_mapping.py
@@ -4,13 +4,13 @@ def test_simple_mapping ():
     print ("S T A R T - T E S T I N G")
 
     # defining paths : Simple ONTOLOGY Mapped
-    mappedPath = "../../lebedigital/SimpleOntology/Mapping/SimpleOntologyMappedProbe1.ttl"  # for current ontology
+    mappedPath = "../lebedigital/SimpleOntology/Mapping/SimpleOntologyMappedProbe1.ttl"  # for current ontology
 
     # defining paths : ONTOLOGY
-    ontoPath = "../../lebedigital/SimpleOntology/SimpleOntology.ttl"
+    ontoPath = "../lebedigital/SimpleOntology/SimpleOntology.ttl"
 
     # defining paths : METADATA
-    dataPath = "../../lebedigital/SimpleOntology/Mapping/probe1.yaml"  # for now only working with this one, more later
+    dataPath = "../lebedigital/SimpleOntology/Mapping/probe1.yaml"  # for now only working with this one, more later
 
     ## loading simple mapping turtle file
     with open(mappedPath, 'r') as file:
