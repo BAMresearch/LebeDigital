5ba5e4721d67ecd7977d972232b41f9efc015ca8
AidaZt <aida.zt61@gmail.com>
2023-02-07 13:56:54 +0100

    change the paths of raw data from lebedigital to usecases

--- tests/mapping/test_simple_mapping.py
+++ tests/mapping/test_simple_mapping.py
@@ -1,16 +1,16 @@
-from lebedigital.SimpleOntology.Mapping.simple_mapping import placeholderreplacement
+from usecases.SimpleOntology.Mapping.simple_mapping import placeholderreplacement
 
 def test_simple_mapping ():
-    print ("S T A R T - T E S T I N G")
+    print("S T A R T - T E S T I N G")
 
     # defining paths : Simple ONTOLOGY Mapped
-    mappedPath = "../lebedigital/SimpleOntology/Mapping/SimpleOntologyMappedProbe1.ttl"  # for current ontology
+    mappedPath = "../usecases/SimpleOntology/Mapping/SimpleOntologyMappedProbe1.ttl"  # for current ontology
 
     # defining paths : ONTOLOGY
-    ontoPath = "../lebedigital/SimpleOntology/SimpleOntology.ttl"
+    ontoPath = "../usecases/SimpleOntology/SimpleOntology.ttl"
 
     # defining paths : METADATA
-    dataPath = "../lebedigital/SimpleOntology/Mapping/probe1.yaml"  # for now only working with this one, more later
+    dataPath = "../usecases/SimpleOntology/Mapping/probe1.yaml"  # for now only working with this one, more later
 
     ## loading simple mapping turtle file
     with open(mappedPath, 'r') as file:
