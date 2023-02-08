51d3607090b3db102ea1f4aac01c6734ff7b87d2
soudehMasoudian <soudeh.masoudian@gmail.com>
2023-02-08 10:03:18 +0100

    change import path

--- tests/mapping/test_simple_mapping.py
+++ tests/mapping/test_simple_mapping.py
@@ -1,16 +1,17 @@
-from usecases.SimpleOntology.Mapping.simple_mapping import placeholderreplacement
+import pytest
 
+from lebedigital.SimpleOntology.simple_mapping import placeholderreplacement
 def test_simple_mapping ():
     print("S T A R T - T E S T I N G")
 
     # defining paths : Simple ONTOLOGY Mapped
-    mappedPath = "../usecases/SimpleOntology/Mapping/SimpleOntologyMappedProbe1.ttl"  # for current ontology
+    mappedPath = "../../usecases/SimpleOntology/Mapping/SimpleOntologyMappedProbe1.ttl"  # for current ontology
 
     # defining paths : ONTOLOGY
-    ontoPath = "../usecases/SimpleOntology/SimpleOntology.ttl"
+    ontoPath = "../../usecases/SimpleOntology/SimpleOntology.ttl"
 
     # defining paths : METADATA
-    dataPath = "../usecases/SimpleOntology/Mapping/probe1.yaml"  # for now only working with this one, more later
+    dataPath = "../../usecases/SimpleOntology/Mapping/probe1.yaml"  # for now only working with this one, more later
 
     ## loading simple mapping turtle file
     with open(mappedPath, 'r') as file:
