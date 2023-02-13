6c83109abe4920d55bac0c342b29fb48c0847d68
Alina Friemel <alina.friemel@bam.de>
2023-02-13 19:58:23 +0100

    edited mapping script for the emodul

--- lebedigital/mapping/emodul_mapping.py
+++ lebedigital/mapping/emodul_mapping.py
@@ -1,254 +1,143 @@
-# standard library
-import os
+# Script for the e-module ontology (extracted from CPTO) to map e-module metadata
+# by reading every line of that ontology and finding/ # replacing the placeholders.
+
+# import libraries
 import yaml
-import datetime
-
-# third party imports
-import numpy as np
-import arviz as az
-from owlready2 import *
-from yaml.loader import SafeLoader
-from rdflib import URIRef, Graph, Literal, BNode
-from rdflib.namespace import FOAF, RDF, XSD
+import os
 from pathlib import Path
-
-#This script is fully dependent on the ontology that on can find at
-#lebedigital/ConcreteOntology/ 
-#Here we generate the Path to get all the ontologies needed to run the script
-#Warnings: If the architecture of the lebedigital change this may need to be changed
-
-#Settings for files and directories:
-ontologyDirName = 'ConcreteOntology'
-lebedigitalPkgName = 'lebedigital'
-baseDir = Path(__file__).parents[2]
-ontologyPath = os.path.join(baseDir, lebedigitalPkgName, ontologyDirName)
-MinWorkingExpDir = os.path.join(baseDir, 'usecases', 'MinimumWorkingExample')
-rawBaseDir = os.path.join(MinWorkingExpDir, 'Data', 'E-modul')
-processedDataDir =os.path.join(MinWorkingExpDir, 'emodul', 'processed_data')
+from loguru import logger
 
 
-def import_metadata(locationOfMetadata):
-    """Import metadata from a file and return it as a dict
-    
-    Parameters
-    ----------
-    locationOfMetadata: string
-        Path to the metadata File
-    
-    Returns
-    -------
-    metaDataDict: dict
-        Dictionary that represent the metadata in the given file"""
-    with open(locationOfMetadata) as metaDataFile:
+def load_metadata(dataPath):
+    '''
+        Load metadata from a given path and return it as dictionary.
+        dataPath : string
+            Path to the metadata yaml-file.
+
+    '''
+
+    with open(dataPath, 'r') as file:
         try:
-            metaDataDict = yaml.load(metaDataFile, Loader=SafeLoader)
-            return metaDataDict
+            metadata = yaml.safe_load(file)
+            return metadata
         except Exception as e:
-            print("Path error: ", file=sys.stderr)
-            print(e, file=sys.stderr)
+            logger.error("Path error: " + str(e))
 
-###############################################################################
-#Utilitys that can be useful for testing if we need to change the script
-def print_ontology_classes(onto):
-    c_list = list(onto.classes())
-    print("List of classes: ")
-    for c in c_list:
-        print(c)
-    print(f"There are '{len(c_list)}' classes")
 
-def print_ontology_instances(onto):
-    i_list = list(onto.individuals())
-    print("List of individuals: ")
-    for i in i_list:
-        print(i)
-    print(f"There are '{len(i_list)}' instancies")
+def placeholderreplacement(
+        ontoPath,
+        metadataPath,
+        outputPath=None
+):
+    '''
+        Maps the values of one given metadata file (for one specimen or
+        experiment) to a given ontology, by searching within ontology linewise
+        for all metadata keys and replacing placeholders with values from the
+        metadata. Also creates and appends an ID for the specimen.
 
-###############################################################################
-def export_knowledge_graph_to_ttl(onto, locationOfKnowledgeGraph):
-    """Export the knowledge graph that we generate by adding instances to our ontology
-    Parameters
-    ----------
-    onto: owlready2 ontology
-        the object containing our knowledge graph
-    locationOfKnowledgeGraph: str
-        Path to the file
-    
-    Returns:
-    --------
-        void
+        Parameter:
+        -----
+        ontoPath : string
+            complete path to Ontology (ttl-format)
+        metadataPath : string
+            complete path to metadata (yaml-format)
+        outputPath : string
+            complete path for output
 
-    Warnings:
-    -Since owlready2 is not used to create knowledge graphs 
-    we need to export it using RDFLIB.
-    -Owlready2 is not designed for knowledge graph generation so using it this 
-    way may result in ambiguous results.
+        Output:
+        ---
+        If no ouput path is given (f.e. for unittesting), the lines will be
+        returned. If the "ontoPath" is given for output, the ontology will
+        be overwritten. To avoid this, give a new name to create a new ttl-file.
 
-    """
-    knowledge_graph = onto.as_rdflib_graph()
-    knowledge_graph.serialize(destination=locationOfKnowledgeGraph, format="turtle")
+    '''
 
-def make_valid_uri_from_string(s):
-    return s.replace('.','_').replace(' ','_').replace(':','_').replace(',','_')
+    # load metadata and get the keys
+    metadata = load_metadata(metadataPath)
+    keys = list(metadata.keys())  
 
-def get_date_time_value(metadata):
-    str_date_time = metadata['operator_date'] + ' ' +metadata['operator_timestamp']
-    date_time = datetime.datetime.strptime(str_date_time, "%d.%m.%Y %H:%M:%S")
-    return date_time
-    
-def generate_knowledge_graph(metadataPath, knowledgeFile):
-    """
-    Generate a knowledge graph
+    # generate ID for the e-module metadata
+    specimenID = "123456ABCD"  # TEMPORARY! replace this example ID with generator
 
-    Parameters
-    ----------
-    metadataPath: str
-        Path to the file containing the metadata
-    knowledgeFile: str
-        Path to the file in which we want to export the knowledge graph
-    
-    Returns
-    -------
-        void
-    """    
-    My_world = World()
+    # read in the ontology as text linewise, creating a list of lines
+    with open(ontoPath, 'r') as file:
+        lines = file.readlines()
 
-    #Load all ontologies
-    cco_ontology = My_world.get_ontology(ontologyPath + "/MergedAllCoreOntology.owl").load()
-    mseo_mid = My_world.get_ontology(ontologyPath + "/MSEO_mid.owl").load()
-    PeriodicTable_ontology = My_world.get_ontology(ontologyPath + "/PeriodicTable.owl").load()
-    WCTmidonto = My_world.get_ontology(ontologyPath + "/WCTmid.owl").load()
-    CSTonto = My_world.get_ontology(ontologyPath + "/ConcreteStressTestOntologie.owl").load()
-    impO = My_world.get_ontology(os.path.join(ontologyPath,'EM.xml')).load()
-    ConcreteMSEO_ontology = My_world.get_ontology(ontologyPath + "/Concrete_Ontology_MSEO.owl").load()
+        # Set up logger
+        logger.debug('S T A R T')
+        logger.debug('File has ' + str(len(lines)) + ' lines.')
+        counter = 0
+        usedKeys = []
 
-    #Get all namespaces
-    bfo = impO.get_namespace("http://purl.obolibrary.org/obo/")
-    cco = impO.get_namespace("http://www.ontologyrepository.com/CommonCoreOntologies/")
-    mseo = impO.get_namespace("https://purl.matolab.org/mseo/mid/")
-    con = impO.get_namespace('http://w3id.org/concrete/#')
-    xml = impO.get_namespace('http://www.w3.org/2001/XMLSchema#')
+        # iterating through the list of lines
+        for i in range(len(lines)):
 
-    #I need to redefined the property because the property was not defined as functional
-    with impO:
-        # infoBearing = cco.InformationBearingEntity
-        # class has_decimal_value(DataProperty):
-        #     domain    = [cco.InformationBearingEntity]
-        #     range     = [float]
-        class has_text_value(DataProperty, FunctionalProperty):
-            domain    = [cco.InformationBearingEntity]
-            range     = [str]
-    #Extract metadata
-    with open(metadataPath) as metadataFile:
-        try:
-            metadata = yaml.load(metadataFile, Loader=SafeLoader)
-            rawPath = os.path.join(rawBaseDir, metadata['experimentName'])
-            processedFile = os.path.join(processedDataDir, metadata['experimentName'])
-            ###########################################################################
-            ########################ADD INDIVIDUALS####################################
-            ###########################################################################
-            
-            #Root
-            concreteSpecimenIndividual = impO.Specimen(make_valid_uri_from_string(metadata['experimentName']))
+            # iterate through list of metadata-keys
+            for key in keys:
 
-            #Individuals needed to give a Diameter value
-            specimenDiameter = cco.Diameter()
-            specimenDiameterValue = cco.InformationBearingEntity()
-            
-            #Individuals for length
-            specimenLength = cco.Length()
-            specimenLengthValue = cco.InformationBearingEntity()
-            
-            #Individuals for Mass
-            specimenMass = cco.Mass()
-            specimenMassValue = cco.InformationBearingEntity()
+                # create placeholder from keyname f.e. "Length_Value"^^xsd:float
+                placeholder = '$$' + key + '_Value$$'
 
-            #Add comment'
-            specimenSecantModulus = ConcreteMSEO_ontology.DeterminationOfSecantModulusOfElasticity(make_valid_uri_from_string(metadata['experimentName']))
+                # if placeholder is in line, replace it with metadata
+                if placeholder in lines[i]:
+                    logger.debug('Found placeholder "' + placeholder + '" for key "' \
+                                 + key + '" with value "' + str(metadata[key]) + '".')
+                    lines[i] = lines[i].replace(placeholder, str(metadata[key]))
+                    counter += 1
+                    usedKeys.append(key)
 
-            #Add filename, filePath of RawData
-            specimenRawDatafile = mseo.RawDataSet()
-            specimenRawFilename =cco.InformationBearingEntity()
-            specimenRawFilePath = cco.InformationBearingEntity()
+                # append the specimen-ID name to "key"_    
+                key_ = key + "_ "
+                if key_ in lines[i]:
+                    lines[i] = lines[i].replace(key_, key + "_" + str(specimenID) + " ")
 
-            #Add date
-            experimentDate = cco.Day()
-            experimentDateValue = cco.InformationBearingEntity()
+            # append the specimen-ID name to "E-ModulTestSpecimen_"
+            if "E-ModulTestSpecimen_ " in lines[i]:
+                logger.debug('Found "E-ModulTestSpecimen_" in line ' + str(i + 1) \
+                             + ' and appended specimen-ID "' + str(specimenID) + '".')    
+                lines[i] = lines[i].replace("E-ModulTestSpecimen_ ", "E-ModulTestSpecimen_" + str(specimenID) + " ")
+                #counter += 1   
+                #usedKeys.append(keys[0])
 
-            #Add operator Name
-            experimentOperator = cco.Agent()
-            experimentOperatorName = cco.DesignativeName()
-            experimentOperatorNameValue = cco.InformationBearingEntity()
 
-            #Add Filename, filePath of Processed Data
-            specimenProcessingOfData = bfo.BFO_0000015()
-            specimenProcessedDatafile = mseo.AnalysedDataSet()
-            specimenProcessedFilename = cco.InformationBearingEntity()
-            specimenProcessedPath = cco.InformationBearingEntity()
-            
-            ###########################################################################
-            #########################ADD PROPERTIES####################################
-            ###########################################################################
-            
-            #Begin Path to SecantDetermination
-            concreteSpecimenIndividual.is_input_of= [specimenSecantModulus]
+    ############################ L O G G I N G #############################
 
-            #Add path to diameter value
-            concreteSpecimenIndividual.RO_0000086 = [specimenDiameter]
-            specimenDiameter.RO_0010001 = [specimenDiameterValue]
-            specimenDiameterValue.has_decimal_value = [float(metadata['diameter'])]
-            specimenDiameterValue.uses_measurement_unit = [cco.MillimeterMeasurementUnit]
-        
-            #Add path to length value
-            concreteSpecimenIndividual.RO_0000086 =[specimenLength]
-            specimenLength.RO_0010001 = [specimenLengthValue]
-            specimenLengthValue.has_decimal_value = [float(metadata['length'])]
-            #Add length unit
-            if(metadata['length_unit'] == "mm"):
-                specimenLengthValue.uses_measurement_unit = [cco.MillimeterMeasurementUnit]
-            
-            #Add path to Mass
-            concreteSpecimenIndividual.RO_0000086 = [specimenMass]
-            specimenMass.RO_0010001 = [specimenMassValue]
-            specimenMassValue.has_decimal_value = [float(metadata['weight'])]
-            #Add unit
-            if(metadata['weight_unit'] == 'g'):
-                specimenMassValue.uses_measurement_unit = [cco.GramMeasurementUnit]
+    unusedKeys = [i for i in keys if i not in usedKeys]
+    logger.debug('Replaced ' + str(counter) + ' placeholders within the ontology.')
+    if len(unusedKeys) > 0:
+        logger.debug('The following ' + str(len(unusedKeys)) + ' of ' + str(len(keys)) \
+                     + ' metadata keys have not been mapped: ')
+        logger.debug(unusedKeys)
+    else:
+        logger.debug('All metadata keys have been mapped.')
 
-            #Add path to Rawfile
-            specimenSecantModulus.has_output = [specimenRawDatafile]
-            specimenRawDatafile.RO_0010001 = [specimenRawFilename, specimenRawFilePath]
-            specimenRawFilename.has_text_value =metadata['experimentName']
-            specimenRawFilePath.has_text_value = rawPath
+    ############################ O U T P U T #############################
+    if outputPath == None:
+        return lines
 
-            #Add path to Processedfile
-            specimenRawDatafile.is_input_of = [specimenProcessingOfData]
-            specimenProcessingOfData.has_output = [specimenProcessedDatafile]
-            specimenProcessedDatafile.RO_0010001 = [specimenProcessedFilename,
-                                                    specimenProcessedPath]
-            specimenProcessedFilename.has_text_value = metadata['experimentName'] + '.csv'
-            specimenProcessedPath.has_text_value = processedDataDir
-            #Add path to date
-            specimenSecantModulus.occures_on = [experimentDate]
-            experimentDate.RO_0010001 = [experimentDateValue]
-            operatorTime = get_date_time_value(metadata)
-            experimentDateValue.has_datetime_value = [operatorTime]
+    else:
+        # saving the list again to the file
+        with open(outputPath, 'w') as file:
+            for line in lines:
+                file.write(line)
 
-            #Add Path to operator
-            specimenSecantModulus.has_agent = [experimentOperator]
-            experimentOperator.designated_by = [experimentOperatorName]
-            experimentOperatorName.RO_0010001 =[experimentOperatorNameValue]
-            experimentOperatorNameValue.has_text_value = metadata['tester_name']
 
-            ###########################################################################
-            #########################Export Graph######################################
-            ###########################################################################
-            
-            export_knowledge_graph_to_ttl(My_world, knowledgeFile)
-        except Exception as e:
-            print("Path error: ", file=sys.stderr)
-            print(e, file=sys.stderr)
-    
 
-# metadataPath = "/home/gilif/BAM/LeBeDigital_Projects/mapping_script_Lebedigital/usecases/MinimumWorkingExample/emodul/metadata_yaml_files/testMetaData.yaml"
 
-# generate_knowledge_graph(owlPath, metadataPath)
+
+
+# T E M P O R A R Y !!!
+# For my personal testing, will be removed later.
+
+# defining paths : ONTOLOGY
+ontoDir = Path(__file__).parents[1]
+ontoFile = "../lebedigital/ConcreteOntology/EModuleOntology.ttl"
+ontoPath = os.path.join(ontoDir, ontoFile)
+
+# defining paths : METADATA
+dataDir = Path(__file__).parents[1]
+dataFile = "../lebedigital/mapping/testMetaData.yaml"  
+dataPath = os.path.join(dataDir, dataFile)
+
+mappedOntoName = os.path.join(Path(__file__).parents[0], 'EmoduleMappedExmpl.ttl')
+placeholderreplacement(ontoPath, dataPath)#, mappedOntoName)
