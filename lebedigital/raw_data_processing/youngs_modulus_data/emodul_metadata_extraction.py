12a811ae4feb08fdedd13c9af37947059db9a40c
Alina Friemel <alina.friemel@bam.de>
2023-02-14 15:56:14 +0100

    changed metadata key-names to fit the ontology placeholders

--- lebedigital/raw_data_processing/youngs_modulus_data/emodul_metadata_extraction.py
+++ lebedigital/raw_data_processing/youngs_modulus_data/emodul_metadata_extraction.py
@@ -52,18 +52,15 @@ def extract_metadata_emodulus(rawDataPath,specimen_file,mix_file):
         # get the name of the folder of the file
         folderName = os.path.basename(rawDataPath)
 
-        # name of experiment is the folder name of the data file
-        metadata['experimentName'] = folderName
-
-
         # read data file line by line
         lines = data.readlines()
 
-        # set software header
-        metadata['software_specification'] = get_metadata_in_one_line(lines[0])[0]
+        # set software header - This data has no placeholder yet.
+        metadata['software_specification'] = get_metadata_in_one_line(lines[0])[0] 
 
         # specific testing machine and software version this script is optimized for
-        assert metadata['software_specification'] == 'MTS793|MPT|DEU|1|2|,|.|:|49|1|1|A'
+        # - This data has no placeholder yet.
+        assert metadata['software_specification'] == 'MTS793|MPT|DEU|1|2|,|.|:|49|1|1|A' 
 
         # get empty lines (where start and end the header)
         emptyLineIndex = []
@@ -73,59 +70,76 @@ def extract_metadata_emodulus(rawDataPath,specimen_file,mix_file):
 
         # service information of the experiment, it should be in between the first two empty lines
         serviceInformation = []
-        for ind in range(emptyLineIndex[0]+1,emptyLineIndex[1]):
+        for ind in range(emptyLineIndex[0]+1,emptyLineIndex[1]+2):
             serviceInformation.append(get_metadata_in_one_line(lines[ind]))
 
-        # get date and time
+
+
+        ###########  D A T A   A B O U T    E X P E R I M E N T  #######
+
+        # name of experiment is the folder name of the data file
+        metadata['experimentName'] = folderName  # This data has no placeholder yet.
+
+        # get experiment date and time
         date, time = serviceInformation[0][4].split(' ')
+        metadata['ExperimentTime'] = str(time) # operator_timestamp
+        metadata['ExperimentDate'] = str(date) # operator_date
 
-        metadata['operator_timestamp'] = str(time)
-        metadata['operator_date'] = str(date)
+        # get measurement duration  
+        metadata['MeasurementDuration'] = float(replace_comma(serviceInformation[10][2]))
 
-        # operator name
-        metadata['tester_name'] = serviceInformation[2][1]
+        # operator name - This data has no placeholder yet.
+        metadata['tester_name'] = serviceInformation[2][1] 
 
-        # name of specimen
-        metadata['specimen_name'] = serviceInformation[3][1]
-
-        # remarks
+        # remarks - This data has no placeholder yet.
         metadata['remark'] = serviceInformation[4][1]
 
-        # weight
+        # set experiment lab location to BAM
+        metadata['Lab'] = 'BAM'
+
+        # set Compression and Transducer Column
+        metadata['CompressionColumn'] = 0
+        metadata['TransducerColumn'] = [1,2,3]
+
+        # set paths
+        metadata['RawDataFile'] = os.path.join(rawDataPath,specimen_file) # path to specimen.dat
+        metadata['ProcessedFile'] = None # path to csv file with values extracted by emodul_generate_processed_data.py
+        try:
+            with open(str(rawDataPath)+'/'+ str(mix_file), encoding="utf8", errors='ignore') as mix_data:
+                lines = mix_data.readlines()
+                lines = lines[0].strip()
+                dataPath = Path(rawDataPath).parents[1]
+                metadata['MixDataFile']= os.path.join(dataPath, "Mischungen", lines)
+        except:
+            metadata['MixDataFile'] = None
+
+
+        ###########  D A T A   A B O U T    S P E C I M E N #######
+
+        # name of specimen - This data has no placeholder yet.
+        metadata['specimen_name'] = serviceInformation[3][1] 
+
+        # set specimen age to 28 days
+        metadata['SpecimenAge'] = 28.0
+
+        # weight - This data has no placeholder yet.
         weight = float(replace_comma(serviceInformation[5][1]))
         metadata['weight'] = weight
 
-        # set weight unit, only a rough approximation
-        # should be included in the input data in the long run
-        if metadata['weight'] > 1000:
-            metadata['weight_unit'] = 'g'
-        elif metadata['weight'] < 10:
-            metadata['weight_unit'] = 'kg'
-        else:
-            raise Exception('Unexpected value of weight')
-
         # set size of specimen
-        metadata['diameter'] = float(replace_comma(serviceInformation[6][1]))
-        metadata['length'] = float(replace_comma(serviceInformation[7][1]))
-
-        if metadata['diameter'] > metadata['length']:
+        metadata['SpecimenDiameter'] = float(replace_comma(serviceInformation[6][1])) #diameter
+        metadata['SpecimenLength'] = float(replace_comma(serviceInformation[7][1])) #length
+        if metadata['SpecimenDiameter'] > metadata['SpecimenLength']:
             dir_name = metadata['experimentName']
             raise Exception(f'Diameter is larger then length, please fix the mistake in {dir_name}')
 
-        # set units, assuming length and diameter have the same
-        if metadata['length'] > 100:
+        # set units, assuming length and diameter have the same - This data has no placeholder yet.
+        if metadata['SpecimenLength'] > 100:
             metadata['length_unit'] = 'mm'
-        elif metadata['length'] < 1:
+        elif metadata['SpecimenLength'] < 1:
             metadata['length_unit'] = 'm'
         else:
             raise Exception('Unexpected value of length')
-
-        try:
-            with open(str(rawDataPath)+'/'+str(mix_file), encoding="utf8", errors='ignore') as mix_data:
-                lines = mix_data.readlines()
-                metadata['mix_file'] = lines[0].strip()
-        except:
-            metadata['mix_file'] = None
 
     return metadata
 
@@ -166,7 +180,8 @@ def main():
     if args.input == None:
         args.input = '../../../usecases/MinimumWorkingExample/Data/E-modul/BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4'
     if args.output == None:
-        args.output = '../../../usecases/MinimumWorkingExample/emodul/metadata_yaml_files/testMetaData.yaml'
+        #args.output = '../../../usecases/MinimumWorkingExample/emodul/metadata_yaml_files/testMetaData.yaml'
+        args.output = '../../../usecases/MinimumWorkingExample/Data/testMetaData.yaml'
 
     # run extraction and write metadata file
     emodul_metadata(args.input, args.output)
