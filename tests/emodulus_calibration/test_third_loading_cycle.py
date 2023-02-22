0849ffcb8bb6db23f180a19de680652e3299f44c
Atul Agrawal <atulagrawal0711@gmail.com>
2023-02-22 11:39:18 +0100

    minor fix to the test_third loading cycle

--- tests/emodulus_calibration/test_third_loading_cycle.py
+++ tests/emodulus_calibration/test_third_loading_cycle.py
@@ -2,9 +2,12 @@ from lebedigital.calibration.utils import extract_third_load_cycle
 
 import numpy
 import pytest
+import os
+from pathlib import Path
 
 def test_third_loading_cycle():
-    path_data = "/home/atul_0711/Documents/PhD_Tasks/LeBeDigital/Codes/ModelCalibration/ModelCalibration/tests/emodulus_calibration/Wolf 8.2 Probe 1.csv"
+    path_data = os.path.join(os.path.dirname(Path(__file__)), 'Wolf 8.2 Probe 1.csv')
+    #path_data = "/home/atul_0711/Documents/PhD_Tasks/LeBeDigital/Codes/ModelCalibration/ModelCalibration/tests/emodulus_calibration/Wolf 8.2 Probe 1.csv"
     extracted_data = extract_third_load_cycle(path_data, threshold=0.5, vizualize=True)
 
     assert numpy.min(extracted_data["Force [kN]"]) == pytest.approx(-118.55971)
