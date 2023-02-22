d5005b2ece22d3f8132414020e3dc596486989f8
Erik Tamsen <git@eriktamsen.com>
2023-02-22 15:31:04 +0100

    fixed prediction test

--- tests/emodulus_calibration/test_prediction.py
+++ tests/emodulus_calibration/test_prediction.py
@@ -9,14 +9,14 @@ def test_prediction():
     print("Prediction test started")
 
     # check if KG is present
-    if os.path.exists(os.path.join(os.path.dirname(Path(__file__)),"calibrationWorkflowWolf 8.2 Probe 1.csv.owl")):
+    if os.path.exists(os.path.join(os.path.dirname(Path(__file__)),"prediction_data.owl")):
         path_calibrated_data = os.path.join(os.path.dirname(Path(__file__)),
-                                            "calibrationWorkflowWolf 8.2 Probe 1.csv.owl")
+                                            "prediction_data.owl")
     else:
         print("The knowledge graph file is not present")
 
     pos_pred = perform_prediction_three_point_bending(knowledge_graph_file=path_calibrated_data, mode="cheap")
     print(f"The posterior predictive mean is {np.mean(pos_pred)} N/mm2")
-    assert np.mean(pos_pred) == pytest.approx(100, rel=0.5)  # this needs to specified by subject matter experts
+    assert np.mean(pos_pred) == pytest.approx(123.05, rel=0.05)  # this needs to specified by subject matter experts
 
     print("Prediction test completed")
