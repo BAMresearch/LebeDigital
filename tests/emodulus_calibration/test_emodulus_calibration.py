2e2cf59d6f7395f7d5ab8248235173bf2cab4f9d
Erik Tamsen <git@eriktamsen.com>
2023-02-23 09:30:08 +0100

    fixing paths for calibration test

--- tests/emodulus_calibration/test_emodulus_calibration.py
+++ tests/emodulus_calibration/test_emodulus_calibration.py
@@ -1,31 +1,31 @@
 from lebedigital.calibration.calibrationWorkflow import esimate_Youngs_modulus
 from lebedigital.calibration.utils import query_KG
-from probeye.ontology.knowledge_graph_import import import_parameter_samples
-
 import pytest
 import numpy as np
-import os
 from pathlib import Path
 
 def test_emodulus_calibration():
+    # defining paths and directories
+    data_dir = 'calibration_data'
+    data_path = Path(__file__).parent / data_dir
+    input_file = "Wolf 8.2 Probe 1.csv"
 
-    # query knowledge graph. Note KG team needs to provide this.
-    print("Calibration test started")
-    output = query_KG(
-        path=os.path.dirname(Path(__file__)), exp_name="Wolf 8.2 Probe 1.csv"
-    )
-    E_samples= esimate_Youngs_modulus(
-        experimental_data=output, calibration_metadata={"E_loc": 30, "E_scale": 10},
-        calibrated_data_path=os.path.dirname(Path(__file__)), mode='cheap'
-    )
+    # defining calibration input
+    E_loc = 30  # TODO (Atul) what is this??? this needs units!
+    E_scale = 10  # TODO (Atul) what is this???
+
+    # query knowledge graph
+    output = query_KG(path=data_path, exp_name=input_file)
+    E_samples = esimate_Youngs_modulus(experimental_data=output,
+                                       calibration_metadata={"E_loc": E_loc, "E_scale": E_scale},
+                                       calibrated_data_path=data_path, mode='cheap')
+
     # checking if the KG file for the calibration is created
-    assert os.path.exists(os.path.join(os.path.dirname(Path(__file__)),
-                          'calibrationWorkflowWolf 8.2 Probe 1.csv.owl')) is True
+    assert (data_path / f'calibrationWorkflow{input_file}.owl').is_file()
 
-    # checking for the calibrated E modulus
-    print(f"The posterior E mean is {np.mean(E_samples)} KN/mm2")
-    assert np.mean(E_samples) == pytest.approx(30, rel=0.4) # this needs to specified by subject matter experts
-
-    print("Calibration test completed")
+    # checking for the mean of the calibrated E modulus
+    # TODO (Atul), there should be a way to make sure the result is the same each time? Defining a random seed?
+    #      rel = 0.4 is too large...
+    assert np.mean(E_samples) == pytest.approx(E_loc, rel=0.4)
 
 
--- tests/emodulus_calibration/test_prediction.py
+++ tests/emodulus_calibration/test_prediction.py
@@ -6,12 +6,17 @@ from pathlib import Path
 
 
 def test_prediction():
+    # defining paths and directories
+    data_dir = 'prediction_data'
+    data_path = Path(__file__).parent / data_dir
+    input_file = 'calibrationWorkflowWolf 8.2 Probe 1.csv.owl'
+    kg_path = data_path / input_file
+
     # check if KG is present
-    kg_path = os.path.join(os.path.dirname(Path(__file__)),'prediction_data/prediction_kg.owl')
-    if not os.path.exists(kg_path):
+    if not kg_path.is_file():
         raise Exception('The knowledge graph file is not present')
 
-    pos_pred = perform_prediction_three_point_bending(knowledge_graph_file=kg_path, mode="cheap")
+    pos_pred = perform_prediction_three_point_bending(knowledge_graph_file=str(kg_path), mode="cheap")
 
-    # TODO carry units till the end!!!
-    assert np.mean(pos_pred) == pytest.approx(125.00497593013911)
+    # TODO carry units till the end (if possible...)!!!
+    assert np.mean(pos_pred) == pytest.approx(119.66482703410645)
