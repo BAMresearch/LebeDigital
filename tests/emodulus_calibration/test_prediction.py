9578ce86c9a124d8a9440b245b4d73c6323cd296
Erik Tamsen <git@eriktamsen.com>
2023-02-23 08:07:26 +0100

    trying to fix prediction test

--- tests/emodulus_calibration/test_prediction.py
+++ tests/emodulus_calibration/test_prediction.py
@@ -6,17 +6,12 @@ from pathlib import Path
 
 
 def test_prediction():
-    print("Prediction test started")
-
     # check if KG is present
-    kg_path = "prediction_data/prediction_kg.owl"
-    if os.path.exists(os.path.join(os.path.dirname(Path(__file__)),kg_path)):
-        path_calibrated_data = os.path.join(os.path.dirname(Path(__file__)),kg_path)
-    else:
-        print("The knowledge graph file is not present")
+    kg_path = os.path.join(os.path.dirname(Path(__file__)),'prediction_data/prediction_kg.owl')
+    if not os.path.exists(kg_path):
+        raise Exception('The knowledge graph file is not present')
 
-    pos_pred = perform_prediction_three_point_bending(knowledge_graph_file=path_calibrated_data, mode="cheap")
-    print(f"The posterior predictive mean is {np.mean(pos_pred)} N/mm2")
-    assert np.mean(pos_pred) == pytest.approx(125.00497593013911)  # this needs to specified by subject matter experts
+    pos_pred = perform_prediction_three_point_bending(knowledge_graph_file=kg_path, mode="cheap")
 
-    print("Prediction test completed")
+    # TODO carry units till the end!!!
+    assert np.mean(pos_pred) == pytest.approx(125.00497593013911)
