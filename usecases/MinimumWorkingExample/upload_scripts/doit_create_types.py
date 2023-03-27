38ce637cb839bea13b498a729c97350250644d72
cezary17 <cezarykujath@gmail.com>
2023-03-27 11:12:24 +0200

    agaub

--- usecases/MinimumWorkingExample/upload_scripts/doit_create_types.py
+++ usecases/MinimumWorkingExample/upload_scripts/doit_create_types.py
@@ -28,7 +28,7 @@ def create_required_sample_types(mixture_directory_path: Union[Path, str],
         return
 
     # connecting to datastore
-    o = Interbis(config['datastore_url'])
+    o = Interbis(config['datastore_url'], verify_certificates=False)
     o.connect_to_datastore(username=config['user'], password=config['pw'])
 
     ingredient_code = config['ingredient_metadata']['ingredient_code']
