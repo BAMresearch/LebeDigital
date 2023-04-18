0f6a462c564b82a0587f7f71b754a7c42fabc7f4
Alina Friemel <alina.friemel@bam.de>
2023-04-18 16:02:44 +0200

    fixing emod-metadata extr testing script

--- tests/raw_data_processing/youngs_modulus_data/test_metadata_extraction_emodul.py
+++ tests/raw_data_processing/youngs_modulus_data/test_metadata_extraction_emodul.py
@@ -14,22 +14,19 @@ def test_metadata_extraction_emodul():
 
         target_data = {"CompressionColumn": 0,
                         "ExperimentDate": "01.09.2014",
-                        "ExperimentName": "BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4",
+                        "ExperimentName": "test_data",
                         "ExperimentTime": '13:25:39',
                         "Lab": "BAM",
                         "MeasurementDuration": 320.02344,
-                        #"MixDataFile": "..\..\..\usecases\MinimumWorkingExample\Data\Mischungen\2014_08_05 Rezeptur_MI.xlsx",
-                        "ProcessedFile": "..//usecases//MinimumWorkingExample//emodul//processed_data",
-                        "RawDataFile": "../../../usecases/MinimumWorkingExample/Data/E-modul/BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4\specimen.dat",
+                        "MixDataFile": "../../../usecases/MinimumWorkingExample/Data/Mischungen/2014_08_05 Rezeptur_MI.xlsx",
+                        "ProcessedFile": "../usecases/MinimumWorkingExample/emodul/processed_data",
+                        "RawDataFile": "../../../usecases/MinimumWorkingExample/Data/E-modul/BA-Losert MI E-Modul 28d v. 04.08.14 Probe 4/specimen.dat",
                         "SpecimenAge": 28.0,
                         "SpecimenDiameter": 98.6,
                         "SpecimenLength": 300.3,
                         "SpecimenName": "BA-Losert E-Modul 28d v. 04.08.14 Probe 4",
                         "SpecimenWeight": 5342.0,
-                        "TransducerColumn":
-                        - 1
-                        - 2
-                        - 3,
+                        "TransducerColumn": [1,2,3],
                         "remark": "Kraftgeregelt 3,9 kN/s",
                         "software_specification": "MTS793|MPT|DEU|1|2|,|.|:|49|1|1|A",
                         "tester_name": "Kh"
@@ -39,3 +36,4 @@ def test_metadata_extraction_emodul():
         test_data = extract_metadata_emodulus(data_path, specimen_file, mix_file)
 
         assert test_data == target_data
+  
