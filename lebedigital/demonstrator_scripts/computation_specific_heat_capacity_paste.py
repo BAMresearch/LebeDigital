479f6672097644ae299fdf8d49240d10a4c13553
Erik Tamsen <git@eriktamsen.com>
2023-06-16 13:12:57 +0200

    fixed computation specific heat paste

--- lebedigital/demonstrator_scripts/computation_specific_heat_capacity_paste.py
+++ lebedigital/demonstrator_scripts/computation_specific_heat_capacity_paste.py
@@ -1,9 +1,17 @@
 from lebedigital.unit_registry import ureg
 
 
-@ureg.wraps("J/kg/K", ("", "", "", "J/kg/K", "J/kg/K", "J/kg/K"))
+@ureg.wraps("J/kg/K", ("", "", "", "J/kg/K", "J/kg/K", "J/kg/K", "kg/m^3", "kg/m^3", "kg/m^3"))
 def computation_specific_heat_capacity_paste(
-    vol_frac_cement, vol_frac_sub, vol_frac_water, shc_cement, shc_sub, shc_water
+    vol_frac_cement,
+    vol_frac_sub,
+    vol_frac_water,
+    shc_cement,
+    shc_sub,
+    shc_water,
+    density_cem,
+    density_sub,
+    density_water,
 ):
     """
     This function computes the specific heat capacity of the cement paste
@@ -22,14 +30,24 @@ def computation_specific_heat_capacity_paste(
         specific heat capacity of substitute (slag)
     shc_water :
         specific heat capacity of water
+    density_cem :
+        density of cement
+    density_sub :
+        density of substitute (slag)
+    density_water :
+        density of water
 
     Returns
     -------
-    specific_heat_capacity_paste : float / pint unit, will be in 'kg_CO2_eq/m^3'
+    specific_heat_capacity_paste : float / pint unit
     """
+    mass_cement = vol_frac_cement * density_cem
+    mass_sub = vol_frac_sub * density_sub
+    mass_water = vol_frac_water * density_water
 
-    specific_heat_capacity_paste = (
-        vol_frac_cement * shc_cement + vol_frac_sub * shc_sub + vol_frac_water * shc_water
-    ) / (vol_frac_cement + vol_frac_sub + vol_frac_water)
+    # average wrt mass
+    specific_heat_capacity_paste = (mass_cement * shc_cement + mass_sub * shc_sub + mass_water * shc_water) / (
+        mass_cement + mass_sub + mass_water
+    )
 
     return specific_heat_capacity_paste
--- tests/demonstrator_scripts/test_computation_specific_heat_capacity_paste.py
+++ tests/demonstrator_scripts/test_computation_specific_heat_capacity_paste.py
@@ -1,9 +1,9 @@
 import pytest
-from pint.testsuite.helpers import \
-    assert_quantity_almost_equal as assert_approx
+from pint.testsuite.helpers import assert_quantity_almost_equal as assert_approx
 
-from lebedigital.demonstrator_scripts.computation_specific_heat_capacity_paste import \
-    computation_specific_heat_capacity_paste
+from lebedigital.demonstrator_scripts.computation_specific_heat_capacity_paste import (
+    computation_specific_heat_capacity_paste,
+)
 from lebedigital.unit_registry import ureg
 
 
@@ -15,10 +15,73 @@ def test_computation_GWP_per_part():
     shc_cement = 1 * ureg("J/kg/K")
     shc_sub = 2 * ureg("J/kg/K")
     shc_water = 3 * ureg("J/kg/K")
+    density_cem = 1 * ureg("kg/m^3")
+    density_sub = 1 * ureg("kg/m^3")
+    density_water = 1 * ureg("kg/m^3")
 
     shc_paste = computation_specific_heat_capacity_paste(
-        vol_frac_cement, vol_frac_sub, vol_frac_water, shc_cement, shc_sub, shc_water
+        vol_frac_cement,
+        vol_frac_sub,
+        vol_frac_water,
+        shc_cement,
+        shc_sub,
+        shc_water,
+        density_cem,
+        density_sub,
+        density_water,
     )
 
     assert shc_paste.magnitude == pytest.approx(2.2, 0.001)
+    assert shc_paste.units == ureg("J/kg/K")
+
+    # test 2
+    vol_frac_cement = 1 / 3 * ureg("")
+    vol_frac_sub = 1 / 3 * ureg("")
+    vol_frac_water = 1 / 3 * ureg("")
+    shc_cement = 1 * ureg("J/kg/K")
+    shc_sub = 1 * ureg("J/kg/K")
+    shc_water = 1 * ureg("J/kg/K")
+    density_cem = 1 * ureg("kg/m^3")
+    density_sub = 1 * ureg("kg/m^3")
+    density_water = 1 * ureg("kg/m^3")
+
+    shc_paste = computation_specific_heat_capacity_paste(
+        vol_frac_cement,
+        vol_frac_sub,
+        vol_frac_water,
+        shc_cement,
+        shc_sub,
+        shc_water,
+        density_cem,
+        density_sub,
+        density_water,
+    )
+
+    assert shc_paste.magnitude == pytest.approx(1, 0.001)
+    assert shc_paste.units == ureg("J/kg/K")
+
+    # test 3
+    vol_frac_cement = 1 / 2 * ureg("")
+    vol_frac_sub = 1 / 4 * ureg("")
+    vol_frac_water = 1 / 4 * ureg("")
+    shc_cement = 2 * ureg("J/kg/K")
+    shc_sub = 1 * ureg("J/kg/K")
+    shc_water = 1 * ureg("J/kg/K")
+    density_cem = 1 * ureg("kg/m^3")
+    density_sub = 1 * ureg("kg/m^3")
+    density_water = 1 * ureg("kg/m^3")
+
+    shc_paste = computation_specific_heat_capacity_paste(
+        vol_frac_cement,
+        vol_frac_sub,
+        vol_frac_water,
+        shc_cement,
+        shc_sub,
+        shc_water,
+        density_cem,
+        density_sub,
+        density_water,
+    )
+
+    assert shc_paste.magnitude == pytest.approx(1.5, 0.001)
     assert shc_paste.units == ureg("J/kg/K")
