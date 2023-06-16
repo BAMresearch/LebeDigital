69cd68c7b4f062eb17cf4bd46a626346445f7cea
Erik Tamsen <git@eriktamsen.com>
2023-06-16 11:49:19 +0200

    simplifed computation sc volume fraction and added test

--- lebedigital/demonstrator_scripts/computation_sc_volume_fraction.py
+++ lebedigital/demonstrator_scripts/computation_sc_volume_fraction.py
@@ -18,16 +18,11 @@ def computation_sc_volume_fraction(input_dic):
         sc_volume_fraction: volume fraction of the substitute (slag/mix2)
     """
 
-    # initialize output dictionary
-    output = {}
-
     if input_dic["sc_mass_fraction"] == 0:
         return 0
     else:
-        mass_sub = 1 * ureg("kg") / (1 + (1 - input_dic["sc_mass_fraction"]) / input_dic["sc_mass_fraction"])
-        print(f"mass_sub = {mass_sub}")
+        mass_sub = input_dic["sc_mass_fraction"] * ureg("kg")
         mass_cem = 1 * ureg("kg") - mass_sub
-        print(f"mass_cem = {mass_cem}")
         vol_sub = mass_sub / input_dic["density_sub"]
         vol_cem = mass_cem / input_dic["density_cem"]
         sc_volume_fraction = vol_sub / (vol_sub + vol_cem)
