135cab34f41b4ff629c2f49e1879c37d38a8cd6e
Erik Tamsen <git@eriktamsen.com>
2023-06-19 14:00:53 +0200

    fix link to files

--- tests/paper_workflow/test_input_output_dict.py
+++ tests/paper_workflow/test_input_output_dict.py
@@ -22,8 +22,12 @@ def test_input_output_dict():
     path_to_input = path_to_workflow / "Inputs"
     path_to_output = path_to_workflow / "Results"
 
-    file_list = [path_to_input / f for f in os.listdir(path_to_input) if f.endswith(".json")]
-    file_list += [path_to_output / f for f in os.listdir(path_to_output) if f.endswith(".json")]
+    file_list = []
+
+    if path_to_input.is_dir():
+        file_list += [path_to_input / f for f in os.listdir(path_to_input) if f.endswith(".json")]
+    if path_to_output.is_dir():
+        file_list += [path_to_output / f for f in os.listdir(path_to_output) if f.endswith(".json")]
 
     dict = {}
     for path in file_list:
