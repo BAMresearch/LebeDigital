e116e9b5bc7c96548c940a06432b988b04a6e687
Atul Agrawal <atulagrawal0711@gmail.com>
2023-06-22 15:16:03 +0200

    fixing seed for reproducibility

--- lebedigital/calibration/calibrationWorkflow.py
+++ lebedigital/calibration/calibrationWorkflow.py
@@ -117,6 +117,10 @@ def estimate_youngs_modulus(
     low_sigma = 0
     high_sigma = 0.005
 
+    # for reproducible results
+    seed =1
+    np.random.seed(seed)
+
     # =========================================
     #       Define the Inference Problem
     # =========================================
