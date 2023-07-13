aba21ea58445cfce32c218e64c4ec8b110b73283
Erik Tamsen <git@eriktamsen.com>
2023-07-13 11:02:29 +0200

    improving dodo

--- usecases/optimization_paper/dodo.py
+++ usecases/optimization_paper/dodo.py
@@ -32,39 +32,34 @@ def paper_plot_target(name):
 
 
 # config doit
-DOIT_CONFIG = {
-    "verbosity": 2,
-}
+DOIT_CONFIG = {"verbosity": 2}
 
 # here is the possibility to implement different doit runs
 # currently the default is set to 'mode=default'
-config = {"mode": get_var("mode", "default")}
-
+# use mode=final to create the final paper with expensive figures etc.
 # the mode or other variables can be used via a dictionary
 # config['mode']
+config = {"mode": get_var("mode", "default")}
 
 # defining relevant paths and directories
 ROOT = pathlib.Path(__file__).parent
 FIGURES_DIR = "figures"  # target folder for all figures
 BAM_PLOT_DIR = "bam_figures"  # origin folder for all BAM figure scripts
-PAPER_DIR = "tex"  # folder for the paper code
+PAPER_DIR = pathlib.Path("tex")  # folder for the paper code
 OPTIMIZATION_DIR = "optimization_workflow"  # folder for the paper code
-
+MACROS_DIR = PAPER_DIR / "macros"  # folder for the paper code
+path_to_optimization_workflows = ROOT / OPTIMIZATION_DIR
 
 # initialize a list of file dependencies for the paper
 PAPER_PLOTS = []
 
-paper_name = "optimization_paper.tex"
-paper_file = ROOT / PAPER_DIR / paper_name
-
-
 # macros
-py_macros_file_BAM = ROOT / PAPER_DIR / "macros" / "py_macros_BAM.tex"
-py_macros_file_TUM = ROOT / PAPER_DIR / "macros" / "py_macros_TUM.tex"
-tex_macros_file_BAM = ROOT / PAPER_DIR / "macros" / "tex_macros_BAM.tex"
-tex_macros_file_TUM = ROOT / PAPER_DIR / "macros" / "tex_macros_TUM.tex"
-py_macros_optimization_file = ROOT / PAPER_DIR / "macros" / "py_macros_optimization_input.tex"
-path_to_optimization_workflows = ROOT / OPTIMIZATION_DIR
+py_macros_file_BAM = ROOT / MACROS_DIR / "py_macros_BAM.tex"
+py_macros_file_TUM = ROOT / MACROS_DIR / "py_macros_TUM.tex"
+tex_macros_file_BAM = ROOT / MACROS_DIR / "tex_macros_BAM.tex"
+tex_macros_file_TUM = ROOT / MACROS_DIR / "tex_macros_TUM.tex"
+py_macros_optimization_file = ROOT / MACROS_DIR / "py_macros_optimization_input.tex"
+
 TEX_MACROS = [
     py_macros_file_BAM,
     py_macros_file_TUM,
@@ -72,7 +67,6 @@ TEX_MACROS = [
     tex_macros_file_TUM,
     py_macros_optimization_file,
 ]
-
 
 # read macros yaml to define figure file names
 with open(py_macros_file_TUM.with_suffix(".yaml")) as f:
@@ -138,16 +132,13 @@ def task_build_design_figures():
     }
 
 
-# homogenization figure
-homogenization_plot_name = data["file_names"][
-    "homogenizationPlot"
-]  # name of output pdf file as defined in macros yaml
-homogenization_plot_script = ROOT / BAM_PLOT_DIR / "create_homogenization_figure.py"
-homogenization_plot_output_file = ROOT / FIGURES_DIR / homogenization_plot_name
-
-
 def task_build_homogenization_figure():
     """build homogenization figure"""
+    # homogenization figure
+    homogenization_plot_name = data["file_names"]["homogenizationPlot"]  # name of pdf file defined in macros yaml
+    homogenization_plot_script = ROOT / BAM_PLOT_DIR / "create_homogenization_figure.py"
+    homogenization_plot_output_file = ROOT / FIGURES_DIR / homogenization_plot_name
+
     target = paper_plot_target(homogenization_plot_name)
 
     return {
@@ -163,16 +154,20 @@ def task_build_homogenization_figure():
     }
 
 
-# homogenization figure
-beam_design_plot_name = data["file_names"]["beamDesignPlot"]  # name of output pdf file as defined in macros yaml
-beam_design_plot_script = ROOT / BAM_PLOT_DIR / "beam_design_plot.py"
-beam_design_plot_output_file = ROOT / FIGURES_DIR / beam_design_plot_name
-
-
 def task_build_beam_design_figure():
     """build beam design figure"""
+    beam_design_plot_name = data["file_names"]["beamDesignPlot"]  # name of output pdf file as defined in macros yaml
+    beam_design_plot_script = ROOT / BAM_PLOT_DIR / "beam_design_plot.py"
+    beam_design_plot_output_file = ROOT / FIGURES_DIR / beam_design_plot_name
+
     target = paper_plot_target(beam_design_plot_name)
-    n = 10  # number of points for figure, 150 should be good for final paper, but takes too long for development
+
+    if config["mode"] == "default":
+        n = 10  # number of points for figure, 150 should be good for final paper, but takes too long for development
+    elif config["mode"] == "final":
+        n = 150
+    else:
+        raise ValueError("Unknown mode")
 
     return {
         "file_dep": [beam_design_plot_script],
@@ -194,7 +189,10 @@ def task_build_tex_macros():
 
 def task_build_optimization_tex_macros():
     """build optimization input and results tex macros"""
+    script = ROOT / MACROS_DIR / "py_macros.py"
+
     return {
+        "file_dep": [script],
         "actions": [(input_optimization_macros, [path_to_optimization_workflows, py_macros_optimization_file])],
         "targets": [py_macros_optimization_file],
         "clean": True,
@@ -203,6 +201,9 @@ def task_build_optimization_tex_macros():
 
 def task_paper():
     """compile pdf from latex source"""
+    paper_name = "optimization_paper.tex"
+    paper_file = ROOT / PAPER_DIR / paper_name
+
     return {
         "file_dep": [paper_file] + TEX_MACROS + PAPER_PLOTS,
         "actions": [f"tectonic {paper_file} -c minimal 2> log_tectonic.txt"],
