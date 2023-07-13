55296b0e93ca8d98bd3cb53668744226f48bd7a0
Erik Tamsen <86715087+eriktamsen@users.noreply.github.com>
2023-07-13 15:16:10 +0200

    92 erik paper branch (#191)
    * split macros and bib file for paper to reduce conflicts
    
    * fix py_macros files
    
    * add resulting py macros to git ignore
    
    * small changes
    
    * fix some warnings
    
    * working on homogenization plots
    
    * homogenization figure in doit
    
    * more changes to homogenization appendix
    
    * working on beam model
    
    * changing beam design output WIP
    
    * working on beam design
    
    * new beam design working in snakemake
    
    * add GWP of steel
    
    * add beam figure to dodo file
    
    * update workflow graph
    
    * more changes for beam in paper
    
    * beam design is working in doit
    
    * changes to input values
    
    * adding conversion of steel gwp input
    
    * added computation paste specific heat capacity
    
    * improved sc input to mass fraction
    
    * fix sc value
    
    * fixed test files
    
    * fixed name of bending beam
    
    * delete test temp files
    
    * remove some temp files with gitignore
    
    * update parameters
    
    * working on function for calibraton of material model
    
    * add json package
    
    * e and fc over time is working
    
    * add test for E and fc over time
    
    * adding alpha_t28d to workflow
    
    * added macros for optimization input
    
    * add inits to include untested files in coverage
    
    * simplifed computation sc volume fraction and added test
    
    * fixed computation specific heat paste
    
    * small fixes mainly comments
    
    * imporve coverage
    
    * improve coverage
    
    * fixed tests
    
    * renaming volume content to mass per cubic meter concrete
    
    * add dummy test for dummy script
    
    * add safety factors
    
    * add loads to workflow
    
    * write test to check output keys of workflow
    
    * fix link to files
    
    * fix link to files
    
    * improve temp constraint
    
    * continue to write paper section on beam and fem
    
    * fix tests
    
    * improve beam design mixed constraint
    
    * improve beam appendix
    
    * working on paper
    
    * continue FEM
    
    * working on heat of hydration figure
    
    * working on introduction
    
    * working on introduction
    
    * working on introduction
    
    * build script for design approach graph
    
    * wip
    
    * workign on paper
    
    * include design approach in doit
    
    * improving doit workflow
    
    * improve doit output
    
    * imporving doit paths
    
    * improving dodo
    
    * working on introduction
    
    * update for analyze kpi
    
    * update for analyze kpi

--- usecases/optimization_paper/dodo.py
+++ usecases/optimization_paper/dodo.py
@@ -4,10 +4,11 @@ import pathlib
 import yaml
 from bam_figures.beam_design_plot import beam_design_plot
 from bam_figures.create_homogenization_figure import create_homogenization_figure
+from bam_figures.design_approach import design_approach_graph
+from bam_figures.paper_workflow_graph import paper_workflow_graph
 from doit import get_var
 from doit.action import CmdAction
-from tex.macros.py_macros import py_macros
-from workflow_graph.paper_workflow_graph import paper_workflow_graph
+from tex.macros.py_macros import input_optimization_macros, py_macros
 
 
 def update(d, u):
@@ -23,45 +24,49 @@ def update(d, u):
     return d
 
 
-DOIT_CONFIG = {
-    "verbosity": 2,
-}
-
-# here is the possibility to implement different doit runs
-# currently the default is set to 'mode=default'
-config = {"mode": get_var("mode", "default")}
-
-# the mode or other variables can be used via a dictionary
-# config['mode']
-
-# general
-ROOT = pathlib.Path(__file__).parent
-figures_dir = "figures"
-
-# initialize a list of file dependencies for the paper
-PAPER_PLOTS = []
-
-
 # function to return target path and append paper dependency list
 def paper_plot_target(name):
-    target = ROOT / figures_dir / name
+    target = ROOT / FIGURES_DIR / name
     PAPER_PLOTS.append(target)
     return target
 
 
-# paper
-paper_dir = "tex"
-paper_name = "optimization_paper.tex"
-paper_file = ROOT / paper_dir / paper_name
+# config doit
+DOIT_CONFIG = {"verbosity": 2}
 
+# here is the possibility to implement different doit runs
+# currently the default is set to 'mode=default'
+# use mode=final to create the final paper with expensive figures etc.
+# the mode or other variables can be used via a dictionary
+# config['mode']
+config = {"mode": get_var("mode", "default")}
+
+# defining relevant paths and directories
+ROOT = pathlib.Path(__file__).parent
+FIGURES_DIR = "figures"  # target folder for all figures
+BAM_PLOT_DIR = "bam_figures"  # origin folder for all BAM figure scripts
+PAPER_DIR = pathlib.Path("tex")  # folder for the paper code
+OPTIMIZATION_DIR = "optimization_workflow"  # folder for the paper code
+MACROS_DIR = PAPER_DIR / "macros"  # folder for the paper code
+path_to_optimization_workflows = ROOT / OPTIMIZATION_DIR
+
+# initialize a list of file dependencies for the paper
+PAPER_PLOTS = []
 
 # macros
-py_macros_file_BAM = ROOT / paper_dir / "macros" / "py_macros_BAM.tex"
-py_macros_file_TUM = ROOT / paper_dir / "macros" / "py_macros_TUM.tex"
-tex_macros_file_BAM = ROOT / paper_dir / "macros" / "tex_macros_BAM.tex"
-tex_macros_file_TUM = ROOT / paper_dir / "macros" / "tex_macros_TUM.tex"
-TEX_MACROS = [py_macros_file_BAM, py_macros_file_TUM, tex_macros_file_BAM, tex_macros_file_TUM]
+py_macros_file_BAM = ROOT / MACROS_DIR / "py_macros_BAM.tex"
+py_macros_file_TUM = ROOT / MACROS_DIR / "py_macros_TUM.tex"
+tex_macros_file_BAM = ROOT / MACROS_DIR / "tex_macros_BAM.tex"
+tex_macros_file_TUM = ROOT / MACROS_DIR / "tex_macros_TUM.tex"
+py_macros_optimization_file = ROOT / MACROS_DIR / "py_macros_optimization_input.tex"
 
+TEX_MACROS = [
+    py_macros_file_BAM,
+    py_macros_file_TUM,
+    tex_macros_file_BAM,
+    tex_macros_file_TUM,
+    py_macros_optimization_file,
+]
 
 # read macros yaml to define figure file names
 with open(py_macros_file_TUM.with_suffix(".yaml")) as f:
@@ -70,15 +75,12 @@ with open(py_macros_file_BAM.with_suffix(".yaml")) as f:
     data = update(data, yaml.load(f, Loader=yaml.SafeLoader))
 
 
-# workflow graph
-workflow_graph_dir = "workflow_graph"
-workflow_graph_name = data["file_names"]["workflowGraph"]  # name of output pdf file as defined in macros yaml
-workflow_graph_script = ROOT / workflow_graph_dir / "paper_workflow_graph.py"
-workflow_output_file = ROOT / figures_dir / workflow_graph_name
-
-
 def task_build_graph():
     """build workflow graph"""
+    # workflow graph
+    workflow_graph_name = data["file_names"]["workflowGraph"]  # name of output pdf file as defined in macros yaml
+    workflow_graph_script = ROOT / BAM_PLOT_DIR / "paper_workflow_graph.py"
+
     target = paper_plot_target(workflow_graph_name)
 
     return {
@@ -92,30 +94,51 @@ def task_build_graph():
 def task_build_snakemake_dag():
     """build snakemake optimization workflow graph"""
     output_file_name = data["file_names"]["snakemakeGraph"]  # name of output pdf file as defined in macros yaml
-    snakemake_dir = "optimization_workflow"
-    snakefile = ROOT / snakemake_dir / "Snakefile"
+    snakefile = ROOT / OPTIMIZATION_DIR / "Snakefile"
 
     target = paper_plot_target(output_file_name)
 
     return {
         "file_dep": [snakefile],
-        "actions": [CmdAction(f"cd {snakemake_dir} && snakemake --forceall --dag | dot -Tpdf > {target}")],
+        "actions": [
+            CmdAction(f"cd {OPTIMIZATION_DIR} && snakemake --forceall --dag 2> log_dag.txt | dot -Tpdf > {target}")
+        ],
         "targets": [target],
         "clean": True,
     }
 
 
-# homogenization figure
-bam_plot_dir = "bam_figures"
-homogenization_plot_name = data["file_names"][
-    "homogenizationPlot"
-]  # name of output pdf file as defined in macros yaml
-homogenization_plot_script = ROOT / bam_plot_dir / "create_homogenization_figure.py"
-homogenization_plot_output_file = ROOT / figures_dir / homogenization_plot_name
+def task_build_design_figures():
+    """build design approach graph"""
+
+    design_apporach_plot_script = ROOT / BAM_PLOT_DIR / "design_approach.py"
+
+    output_file_name_1 = data["file_names"]["designStandard"]  # name of output pdf file as defined in macros yaml
+    output_file_name_2 = data["file_names"]["designProposed"]  # name of output pdf file as defined in macros yaml
+
+    target1 = paper_plot_target(output_file_name_1)
+    target2 = paper_plot_target(output_file_name_2)
+
+    return {
+        "file_dep": [design_apporach_plot_script],
+        "actions": [
+            (
+                design_approach_graph,
+                [target1.with_suffix("").with_suffix(""), target2.with_suffix("").with_suffix("")],
+            )
+        ],
+        "targets": [target1, target2],
+        "clean": True,
+    }
 
 
 def task_build_homogenization_figure():
     """build homogenization figure"""
+    # homogenization figure
+    homogenization_plot_name = data["file_names"]["homogenizationPlot"]  # name of pdf file defined in macros yaml
+    homogenization_plot_script = ROOT / BAM_PLOT_DIR / "create_homogenization_figure.py"
+    homogenization_plot_output_file = ROOT / FIGURES_DIR / homogenization_plot_name
+
     target = paper_plot_target(homogenization_plot_name)
 
     return {
@@ -131,16 +154,20 @@ def task_build_homogenization_figure():
     }
 
 
-# homogenization figure
-beam_design_plot_name = data["file_names"]["beamDesignPlot"]  # name of output pdf file as defined in macros yaml
-beam_design_plot_script = ROOT / bam_plot_dir / "beam_design_plot.py"
-beam_design_plot_output_file = ROOT / figures_dir / beam_design_plot_name
-
-
 def task_build_beam_design_figure():
     """build beam design figure"""
+    beam_design_plot_name = data["file_names"]["beamDesignPlot"]  # name of output pdf file as defined in macros yaml
+    beam_design_plot_script = ROOT / BAM_PLOT_DIR / "beam_design_plot.py"
+    beam_design_plot_output_file = ROOT / FIGURES_DIR / beam_design_plot_name
+
     target = paper_plot_target(beam_design_plot_name)
-    n = 10  # number of points for figure, 150 should be good for final paper, but takes too long for development
+
+    if config["mode"] == "default":
+        n = 10  # number of points for figure, 150 should be good for final paper, but takes too long for development
+    elif config["mode"] == "final":
+        n = 150
+    else:
+        raise ValueError("Unknown mode")
 
     return {
         "file_dep": [beam_design_plot_script],
@@ -160,11 +187,26 @@ def task_build_tex_macros():
     }
 
 
+def task_build_optimization_tex_macros():
+    """build optimization input and results tex macros"""
+    script = ROOT / MACROS_DIR / "py_macros.py"
+
+    return {
+        "file_dep": [script],
+        "actions": [(input_optimization_macros, [path_to_optimization_workflows, py_macros_optimization_file])],
+        "targets": [py_macros_optimization_file],
+        "clean": True,
+    }
+
+
 def task_paper():
     """compile pdf from latex source"""
+    paper_name = "optimization_paper.tex"
+    paper_file = ROOT / PAPER_DIR / paper_name
+
     return {
         "file_dep": [paper_file] + TEX_MACROS + PAPER_PLOTS,
-        "actions": [f"tectonic {paper_file}"],  # AA: I need to add./tectonic
+        "actions": [f"tectonic {paper_file} -c minimal 2> log_tectonic.txt"],
         "targets": [paper_file.with_suffix(".pdf")],
         "clean": True,
     }
