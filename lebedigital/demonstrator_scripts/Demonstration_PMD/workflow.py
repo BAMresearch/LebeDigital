37c54a5fb428d27cc18f02143ae8fd5006fcb297
Alina Friemel <alina.friemel@bam.de>
2023-07-17 14:26:28 +0200

    updated workflow, containing now also mapping

--- lebedigital/demonstrator_scripts/Demonstration_PMD/workflow.py
+++ lebedigital/demonstrator_scripts/Demonstration_PMD/workflow.py
@@ -1,14 +1,29 @@
 from import_gui import main
 from lebedigital.raw_data_processing.mixture.mixdesign_metadata_extraction import mix_metadata
 from lebedigital.raw_data_processing.youngs_modulus_data.emodul_metadata_extraction import emodul_metadata
+from lebedigital.mapping.mappingscript import mapping
 
 # Script that takes raw data paths as input via GUI and has as output different knowledge graph ttl-files
 
-# start GUI to retrieve paths for raw data as dicitionary, one file for each mixture and for e-module
-paths = main()
+# define output paths for the metadata and paths to Knowledge Graph templates
+metadataPaths = {'Mixture': '../../../usecases/MinimumWorkingExample/mixture/metadata_json_files/Workflow2014_08_05 Rezeptur_MI.json',
+            'Module:' : '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestMetaData.json',
+            'Specimen': '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestSpecimenData.json'}
+
+KGtemplatePaths = {'Mixture' : '../../lebedigital/ConcreteOntology/MixtureDesign_KG_Template.ttl',
+                   'Module' : '../../lebedigital/ConcreteOntology/EModuleOntology_KG_Template.ttl',
+                   'Specimen' : '../../lebedigital/ConcreteOntology/Specimen_KG_Template.ttl'}
+
+# start GUI to retrieve paths for raw data as dictionary, one file for each mixture and for e-module
+rawPaths = main()
 
 # run metadata extraction script for mixDesign
-mix_metadata(paths['Mixture'], '../../../usecases/MinimumWorkingExample/mixture/metadata_json_files/Workflow2014_08_05 Rezeptur_MI.json')
+mix_metadata(rawPaths['Mixture'], metadataPaths['Mixture'])
 
 # run metadata extraction script for e-module (simultaneously creating KG for specimen)
-emodul_metadata(paths['Module'], '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestMetaData.json','../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestSpecimenData.json')
+emodul_metadata(rawPaths['Module'], metadataPaths['Module'], metadataPaths['Specimen'])
+
+# run the mapping script for each extracted metadata
+mapping(metadataPaths['Mixture'], KGtemplatePaths['Mixture'])
+mapping(metadataPaths['Module'], KGtemplatePaths['Module'])
+mapping(metadataPaths['Specimen'], KGtemplatePaths['Specimen'])
