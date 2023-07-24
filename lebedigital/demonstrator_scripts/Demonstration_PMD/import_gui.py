5e89a8c1947d35ef5eb23033eb3857b909bbf969
AidaZt <aida.zt61@gmail.com>
2023-07-24 11:20:02 +0200

    update GUI and Workflow

--- lebedigital/demonstrator_scripts/Demonstration_PMD/import_gui.py
+++ lebedigital/demonstrator_scripts/Demonstration_PMD/import_gui.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt5.QtGui import QFont
+
+from PyQt5.QtGui import QFont, QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel, QVBoxLayout, QPushButton, QComboBox, QWidget
 from PyQt5.QtCore import pyqtSlot
 import os
@@ -12,6 +13,7 @@ class App(QMainWindow):
         self.setWindowTitle('Converter')
         self.setMinimumSize(400, 300)
         self.dataDict = {}
+
 
         mainWidget = QWidget(self)
         self.setCentralWidget(mainWidget)
@@ -31,10 +33,11 @@ class App(QMainWindow):
         layout.addWidget(self.label2)
 
         self.comboBox = QComboBox(self)
-        self.comboBox.addItem("E-Module")
-        self.comboBox.addItem("Compressive Strength")
-        self.comboBox.addItem("Mixture Design")
+        #self.comboBox.addItem("E-Module")
+        #self.comboBox.addItem("Compressive Strength")
+        #self.comboBox.addItem("Mixture Design")
         layout.addWidget(self.comboBox)
+
 
         self.btn = QPushButton('Datei auswählen', self)
         self.btn.clicked.connect(self.openFileNameDialog)
@@ -48,6 +51,27 @@ class App(QMainWindow):
         layout.addStretch()
         layout.addWidget(self.doneButton)
 
+        # Create the item model
+        self.item_model = QStandardItemModel(self)
+        self.comboBox.setModel(self.item_model)
+
+        # Add main items to the QComboBox
+        self.module_item = QStandardItem("E-Module")
+        self.mixture_item = QStandardItem("Mixture Design")
+        self.compress_item = QStandardItem("Compressive Strength")
+
+        self.item_model.appendRow(self.module_item)
+        self.item_model.appendRow(self.mixture_item)
+        self.item_model.appendRow(self.compress_item)
+
+        # Add subitems only for "E-Module" and make them checkable
+        mix_subitem = QStandardItem("mix")
+        mix_subitem.setCheckable(True)
+        specimen_subitem = QStandardItem("Specimen")
+        specimen_subitem.setCheckable(True)
+        self.module_item.appendRow(mix_subitem)
+        self.module_item.appendRow(specimen_subitem)
+
     @pyqtSlot()
     def openFileNameDialog(self):
         options = QFileDialog.Options()
@@ -56,9 +80,21 @@ class App(QMainWindow):
                                                   "Excel Files (*.xlsx *.csv *.dat)", options=options)
         if fileName:
             dataset = self.comboBox.currentText()
-            self.dataDict[dataset] = fileName
+            #selected_subitems = [self.module_item.child(row).text() for row in range(self.module_item.rowCount())
+                                 #if self.module_item.child(row).checkState()]
+
+            if dataset not in self.dataDict:
+                self.dataDict[dataset] = []
+                #print('check value')
+                #print(selected_subitems)
+            #self.dataDict[dataset].append((fileName, fileName.split("/")[-1]))
+            self.dataDict[dataset].append(fileName)
+
+            #self.dataDict[dataset] = fileName
             self.filePathLabel.setText(self.filePathLabel.text() + "\n" + os.path.basename(fileName))
-            self.comboBox.removeItem(self.comboBox.currentIndex())
+
+
+            #self.comboBox.removeItem(self.comboBox.currentIndex())
 
     @pyqtSlot()
     def closeWindow(self):
@@ -70,11 +106,19 @@ def main():
     ex = App()
     ex.show()
     app.exec_()
-    new_dict = {}
-    for key, value in ex.dataDict.items():
-        new_dict[key] = value.rpartition('/')[0]
 
-    return new_dict
+
+    # Filter out entries with empty lists from dataDict
+    #filtered_data_dict = {key: value for key, value in ex.dataDict.items() if value}
+
+    #return filtered_data_dict
+
+    return ex.dataDict
+    #new_dict = {}
+    #for key, value in ex.dataDict.items():
+        #new_dict.update({key: (str(value[0]), str(value[1]))})
+
+    #return new_dict
 
 
 if __name__ == '__main__':
--- lebedigital/demonstrator_scripts/Demonstration_PMD/workflow.py
+++ lebedigital/demonstrator_scripts/Demonstration_PMD/workflow.py
@@ -1,29 +1,32 @@
 from import_gui import main
-from lebedigital.raw_data_processing.mixture.mixdesign_metadata_extraction import mix_metadata
-from lebedigital.raw_data_processing.youngs_modulus_data.emodul_metadata_extraction import emodul_metadata
-from lebedigital.mapping.mappingscript import mapping
+from Lebedigital.lebedigital.raw_data_processing.mixture.mixdesign_metadata_extraction import mix_metadata
+from Lebedigital.lebedigital.raw_data_processing.youngs_modulus_data.emodul_metadata_extraction import emodul_metadata
+from Lebedigital.lebedigital.mapping.mappingscript import mapping
 
 # Script that takes raw data paths as input via GUI and has as output different knowledge graph ttl-files
 
 # define output paths for the metadata and paths to Knowledge Graph templates
-metadataPaths = {'Mixture': '../../../usecases/MinimumWorkingExample/mixture/metadata_json_files/Workflow2014_08_05 Rezeptur_MI.json',
-            'Module:' : '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestMetaData.json',
-            'Specimen': '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestSpecimenData.json'}
+metadataPaths = {'Mixture_Metadata': '../../../usecases/MinimumWorkingExample/mixture/metadata_json_files/Workflow2014_08_05 Rezeptur_MI.json',
+            'Module_Metadata': '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestMetaData.json',
+            'Specimen_Metadata': '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestSpecimenData.json'}
 
-KGtemplatePaths = {'Mixture' : '../../lebedigital/ConcreteOntology/MixtureDesign_KG_Template.ttl',
-                   'Module' : '../../lebedigital/ConcreteOntology/EModuleOntology_KG_Template.ttl',
-                   'Specimen' : '../../lebedigital/ConcreteOntology/Specimen_KG_Template.ttl'}
+
+MappedKGtemplate = {'MixMapped': '../../../usecases/MinimumWorkingExample/Mapping_Example/testMixMapped.ttl'}
+# define input paths to Knowledge Graph templates
+KGtemplatePaths = {'Mixture_KG': '../../../lebedigital/ConcreteOntology/MixtureDesign_KG_Template.ttl',
+                   'Module_KG': '../../../lebedigital/ConcreteOntology/EModuleOntology_KG_Template.ttl',
+                   'Specimen_KG': '../../../lebedigital/ConcreteOntology/Specimen_KG_Template.ttl'}
 
 # start GUI to retrieve paths for raw data as dictionary, one file for each mixture and for e-module
 rawPaths = main()
 
 # run metadata extraction script for mixDesign
-mix_metadata(rawPaths['Mixture'], metadataPaths['Mixture'])
+mix_metadata(rawPaths['Mixture Design'], metadataPaths['Mixture_Metadata'])
 
 # run metadata extraction script for e-module (simultaneously creating KG for specimen)
-emodul_metadata(rawPaths['Module'], metadataPaths['Module'], metadataPaths['Specimen'])
+#emodul_metadata(rawPaths['E-Module'], metadataPaths['Module_Metadata'], metadataPaths['Specimen_Metadata'])
 
 # run the mapping script for each extracted metadata to generate three KGs
-mapping(metadataPaths['Mixture'], KGtemplatePaths['Mixture'])
-mapping(metadataPaths['Module'], KGtemplatePaths['Module'])
-mapping(metadataPaths['Specimen'], KGtemplatePaths['Specimen'])
+mapping(KGtemplatePaths['Mixture_KG'], metadataPaths['Mixture_Metadata'], MappedKGtemplate['MixMapped'])
+#mapping(metadataPaths['Module_Metadata'], KGtemplatePaths['Module_KG'])
+#mapping(metadataPaths['Specimen_Metadata'], KGtemplatePaths['Specimen_KG'])
