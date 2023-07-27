9e9726f6ee6b69f4a96bf6a2b900a95443c19bf1
AidaZt <aida.zt61@gmail.com>
2023-07-27 16:17:42 +0200

    update workflow

--- lebedigital/demonstrator_scripts/Demonstration_PMD/workflow.py
+++ lebedigital/demonstrator_scripts/Demonstration_PMD/workflow.py
@@ -4,16 +4,15 @@ from Lebedigital.lebedigital.raw_data_processing.youngs_modulus_data.emodul_meta
 from Lebedigital.lebedigital.mapping.mappingscript import mapping
 
 # Script that takes raw data paths as input via GUI and has as output different knowledge graph ttl-files
-
 # define output paths for the metadata and paths to Knowledge Graph templates
-metadataPaths = {'Mixture_Metadata': '../../../usecases/MinimumWorkingExample/mixture/metadata_json_files/Workflow2014_08_05 Rezeptur_MI.json',
-            'Module_Metadata': '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestMetaData.json',
-            'Specimen_Metadata': '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/WorkflowtestSpecimenData.json'}
+metadataPaths = {'Mixture_Metadata': '../../../usecases/MinimumWorkingExample/mixture/metadata_json_files/',
+            'Module_Metadata': '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/testMetaData.json',
+            'Specimen_Metadata': '../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/testSpecimenData.json'}
 
 
 MappedKGtemplate_mix = {'MixMapped': '../../../usecases/MinimumWorkingExample/Mapping_Example/testMixMapped.ttl'}
 MappedKGtemplate_EModule = {'EModuleMapped': '../../../usecases/MinimumWorkingExample/Mapping_Example/testMapped.ttl'}
-MappedKGtemplate_Specimen = {'SpecimenMapped': '../../../usecases/MinimumWorkingExample/Mapping_Example/testMapped.ttl'}
+MappedKGtemplate_Specimen = {'SpecimenMapped': '../../../usecases/MinimumWorkingExample/Mapping_Example/testSpecimenMapped.ttl'}
 
 # define input paths to Knowledge Graph templates
 KGtemplatePaths = {'Mixture_KG': '../../../lebedigital/ConcreteOntology/MixtureDesign_KG_Template.ttl',
@@ -24,12 +23,16 @@ KGtemplatePaths = {'Mixture_KG': '../../../lebedigital/ConcreteOntology/MixtureD
 rawPaths = main()
 
 # run metadata extraction script for mixDesign
-mix_metadata(rawPaths['Mixture Design'], metadataPaths['Mixture_Metadata'])
+path_to_json = mix_metadata(rawPaths['Mixture Design'], metadataPaths['Mixture_Metadata'])
 
 # run metadata extraction script for e-module (simultaneously creating KG for specimen)
-emodul_metadata(rawPaths['E-Module'], metadataPaths['Module_Metadata'], metadataPaths['Specimen_Metadata'])
+emodul_metadata(rawPaths['E-Module'], metadataPaths['Module_Metadata'], metadataPaths['Specimen_Metadata'], path_to_json)
 
 # run the mapping script for each extracted metadata to generate three KGs
-mapping(KGtemplatePaths['Mixture_KG'], metadataPaths['Mixture_Metadata'], MappedKGtemplate_mix['MixMapped'])
-mapping(metadataPaths['Module_Metadata'], KGtemplatePaths['Module_KG'], MappedKGtemplate_EModule['EModuleMapped'])
-mapping(metadataPaths['Specimen_Metadata'], KGtemplatePaths['Specimen_KG'], MappedKGtemplate_Specimen['SpecimenMapped'])
+#mapping(KGtemplatePaths['Mixture_KG'], metadataPaths['Mixture_Metadata'], MappedKGtemplate_mix['MixMapped'])
+mapping(KGtemplatePaths['Mixture_KG'], path_to_json, MappedKGtemplate_mix['MixMapped'])
+mapping(KGtemplatePaths['Specimen_KG'], metadataPaths['Specimen_Metadata'], MappedKGtemplate_Specimen['SpecimenMapped'])
+mapping(KGtemplatePaths['Module_KG'], metadataPaths['Module_Metadata'], MappedKGtemplate_EModule['EModuleMapped'])
+
+
+
