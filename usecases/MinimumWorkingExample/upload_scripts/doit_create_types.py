2454a801faeca2a513431f3ee95afdccabd73eb4
cezary17 <cezarykujath@gmail.com>
2023-11-17 17:05:20 +0100

    removed Interbis, added ExtPybis

--- usecases/MinimumWorkingExample/upload_scripts/doit_create_types.py
+++ usecases/MinimumWorkingExample/upload_scripts/doit_create_types.py
@@ -6,40 +6,48 @@ from typing import Union
 import yaml
 from pybis.entity_type import SampleType
 
-from lebedigital.openbis.interbis import Interbis
+from extpybis.openbis import ExtOpenbis
 
 conv_dict = {
-    str: 'VARCHAR',
-    float: 'REAL',
-    int: 'INTEGER',
+    str: "VARCHAR",
+    float: "REAL",
+    int: "INTEGER",
 }
 
 
-def create_required_sample_types(mixture_directory_path: Union[Path, str],
-                                 emodul_directory_path: Union[Path, str],
-                                 config: dict,
-                                 default_props: dict,
-                                 logging_path: Union[Path, str],):
-
-    if config['runson'] == 'nodb':
-        _create_logfiles(mixture_sample_type="RAN WITHOUT OPENBIS CONNECTION",
-                         emodul_sample_type="RAN WITHOUT OPENBIS CONNECTION",
-                         ingredient_sample_type="RAN WITHOUT OPENBIS CONNECTION",
-                         logging_path=logging_path)
+def create_required_sample_types(
+    mixture_directory_path: Union[Path, str],
+    emodul_directory_path: Union[Path, str],
+    config: dict,
+    default_props: dict,
+    logging_path: Union[Path, str],
+):
+    if config["runson"] == "nodb":
+        _create_logfiles(
+            mixture_sample_type="RAN WITHOUT OPENBIS CONNECTION",
+            emodul_sample_type="RAN WITHOUT OPENBIS CONNECTION",
+            ingredient_sample_type="RAN WITHOUT OPENBIS CONNECTION",
+            logging_path=logging_path,
+        )
         return
 
     # connecting to datastore
-    o = Interbis(config['datastore_url'], verify_certificates=False if config['runson'] == 'docker' else True)
-    o.connect_to_datastore(username=config['user'], password=config['pw'])
+    o = ExtOpenbis(
+        config["datastore_url"],
+        verify_certificates=False if config["runson"] == "docker" else True,
+    )
+    o.connect_to_datastore(username=config["user"], password=config["pw"])
 
-    ingredient_code = config['ingredient_metadata']['ingredient_code']
-    ingredient_prefix = config['ingredient_metadata']['ingredient_prefix']
-    ingredient_props = config['ingredient_metadata']['ingredient_props']
-    ingredient_keywords = config['ingredient_metadata']['ingredient_keywords']
+    ingredient_code = config["ingredient_metadata"]["ingredient_code"]
+    ingredient_prefix = config["ingredient_metadata"]["ingredient_prefix"]
+    ingredient_props = config["ingredient_metadata"]["ingredient_props"]
+    ingredient_keywords = config["ingredient_metadata"]["ingredient_keywords"]
 
-    ingredient_sample_type = o.create_sample_type(ingredient_code, ingredient_prefix, ingredient_props)
+    ingredient_sample_type = o.create_sample_type(
+        ingredient_code, ingredient_prefix, ingredient_props
+    )
 
-    ingredient_hint_props = config['ingredient_metadata']['ingredient_hint_props']
+    ingredient_hint_props = config["ingredient_metadata"]["ingredient_hint_props"]
     ingredient_hint_created_props = o.create_property_types(ingredient_hint_props)
 
     ingredient_hint_created_props = list(ingredient_hint_created_props.keys())
@@ -50,46 +58,60 @@ def create_required_sample_types(mixture_directory_path: Union[Path, str],
         yaml_directory_path=Path(mixture_directory_path),
         output_path=None,
         mixture_code=f"EXPERIMENTAL_STEP_{config['mixture_prefix']}",
-        defaults_dict=default_props)
+        defaults_dict=default_props,
+    )
 
     # filtering out ingredient_keywords
-    water_cement_ratio_prop = mix_union_dict.pop(f"EXPERIMENTAL_STEP_{config['mixture_prefix']}.water_cement_ratio")
-    filtered_mix_union_dict = {key: val for key, val in mix_union_dict.items() if not [keyword for keyword in ingredient_keywords if keyword in key]}
-    filtered_mix_union_dict[f"EXPERIMENTAL_STEP_{config['mixture_prefix']}.water_cement_ratio"] = water_cement_ratio_prop
+    water_cement_ratio_prop = mix_union_dict.pop(
+        f"EXPERIMENTAL_STEP_{config['mixture_prefix']}.water_cement_ratio"
+    )
+    filtered_mix_union_dict = {
+        key: val
+        for key, val in mix_union_dict.items()
+        if not [keyword for keyword in ingredient_keywords if keyword in key]
+    }
+    filtered_mix_union_dict[
+        f"EXPERIMENTAL_STEP_{config['mixture_prefix']}.water_cement_ratio"
+    ] = water_cement_ratio_prop
 
     mixture_sample_type = o.create_sample_type(
         sample_code=f"EXPERIMENTAL_STEP_{config['mixture_prefix']}",
-        sample_prefix=config['mixture_prefix'],
+        sample_prefix=config["mixture_prefix"],
         sample_properties=filtered_mix_union_dict,
     )
-    logging.debug(f'mixture type created: {mixture_sample_type.code}')
+    logging.debug(f"mixture type created: {mixture_sample_type.code}")
 
     # filtered_ingredient_union_list = [key.split('--')[0].split('.')[1] for key in mix_union_dict if key not in filtered_mix_union_dict]
     # set_ingredients = list(set(filtered_ingredient_union_list))
     # logging.warning(set_ingredients)
 
-    annotations = [{
-        "TYPE": ingredient_hint_created_props[0],  # quantity in mix
-        "MANDATORY": True,
-    },
+    annotations = [
         {
-        "TYPE": ingredient_hint_created_props[1],  # volume
-        "MANDATORY": False
-    }]
+            "TYPE": ingredient_hint_created_props[0],  # quantity in mix
+            "MANDATORY": True,
+        },
+        {
+            "TYPE": ingredient_hint_created_props[1],  # volume
+            "MANDATORY": False,
+        },
+    ]
 
     o.create_parent_hint(
         sample_type=mixture_sample_type,
         label="Emodul Raw Material",
         parent_type=ingredient_sample_type,
         annotation_properties=annotations,
-        min_count=2
+        min_count=2,
     )
 
-    metadata_path = Path(emodul_directory_path, os.fsdecode(os.listdir(emodul_directory_path)[0]))
+    metadata_path = Path(
+        emodul_directory_path, os.fsdecode(os.listdir(emodul_directory_path)[0])
+    )
 
     # CREATING EMODUL SAMPLE TYPE from single yaml (all equal)
-    emodul_metadata_type_dict = _read_metadata_for_types(metadata_path, f"EXPERIMENTAL_STEP_{config['emodul_prefix']}",
-                                                         default_props)
+    emodul_metadata_type_dict = _read_metadata_for_types(
+        metadata_path, f"EXPERIMENTAL_STEP_{config['emodul_prefix']}", default_props
+    )
 
     # CREATING VOCABULARIES
     _create_unit_vocabularies(o)
@@ -97,28 +119,39 @@ def create_required_sample_types(mixture_directory_path: Union[Path, str],
     # Creating the emodul sample type with the formatted dict
     emodul_sample_type = o.create_sample_type(
         sample_code=f"EXPERIMENTAL_STEP_{config['emodul_prefix']}",
-        sample_prefix=config['emodul_prefix'],
+        sample_prefix=config["emodul_prefix"],
         sample_properties=emodul_metadata_type_dict,
     )
-    logging.debug(f'emodul type created: {emodul_sample_type.code}')
+    logging.debug(f"emodul type created: {emodul_sample_type.code}")
 
-    _create_logfiles(mixture_sample_type=mixture_sample_type.code,
-                     emodul_sample_type=emodul_sample_type.code,
-                     ingredient_sample_type=ingredient_sample_type.code,
-                     logging_path=logging_path)
+    _create_logfiles(
+        mixture_sample_type=mixture_sample_type.code,
+        emodul_sample_type=emodul_sample_type.code,
+        ingredient_sample_type=ingredient_sample_type.code,
+        logging_path=logging_path,
+    )
 
     o.logout()
 
 
-def _create_union_dict(yaml_directory_path: Path, output_path: Union[Path, None], mixture_code: str,
-                       defaults_dict: dict) -> dict:
+def _create_union_dict(
+    yaml_directory_path: Path,
+    output_path: Union[Path, None],
+    mixture_code: str,
+    defaults_dict: dict,
+) -> dict:
     union_dict = defaults_dict
     for file in os.scandir(yaml_directory_path):
         with open(file, "r") as stream:
-            current_dict = {key: val for key, val in dict(yaml.safe_load(stream)).items()
-                            if key not in union_dict.keys()}
-            current_dict = {f"{mixture_code}.{key}": [conv_dict[type(val)], key, key]
-                            for key, val in current_dict.items()}
+            current_dict = {
+                key: val
+                for key, val in dict(yaml.safe_load(stream)).items()
+                if key not in union_dict.keys()
+            }
+            current_dict = {
+                f"{mixture_code}.{key}": [conv_dict[type(val)], key, key]
+                for key, val in current_dict.items()
+            }
         union_dict = dict(current_dict, **union_dict)
 
     if output_path:
@@ -129,7 +162,9 @@ def _create_union_dict(yaml_directory_path: Path, output_path: Union[Path, None]
     return union_dict
 
 
-def _read_metadata_for_types(yaml_path: Union[str, Path], sample_type_code: str, default_props: dict):
+def _read_metadata_for_types(
+    yaml_path: Union[str, Path], sample_type_code: str, default_props: dict
+):
     """
     Reads the metadata from a single yaml file and converts the dict to a format accepted by the create sample type
     method
@@ -143,35 +178,49 @@ def _read_metadata_for_types(yaml_path: Union[str, Path], sample_type_code: str,
 
     """
     default_keys = default_props.keys()
-    with open(yaml_path, 'r') as file:
+    with open(yaml_path, "r") as file:
         loaded = dict(yaml.safe_load(file))
         output_dict = default_props
         for key, val in loaded.items():
             if key not in default_keys:
                 val = "" if val is None else val
-                output_dict[f"{sample_type_code}.{key}".lower()] = [conv_dict[type(val)], key.split('.')[-1],
-                                                                    key.split('.')[-1]]
+                output_dict[f"{sample_type_code}.{key}".lower()] = [
+                    conv_dict[type(val)],
+                    key.split(".")[-1],
+                    key.split(".")[-1],
+                ]
 
         # Tweaking the dict to use our defined controlledvocabulary instead of varchar for units
-        output_dict[f"{sample_type_code}.length_unit".lower()] = ['CONTROLLEDVOCABULARY', 'length_unit', 'length_unit',
-                                                                  'LENGTH_VOCAB']
-        output_dict[f"{sample_type_code}.weight_unit".lower()] = ['CONTROLLEDVOCABULARY', 'weight_unit', 'weight_unit',
-                                                                  'WEIGHT_VOCAB']
+        output_dict[f"{sample_type_code}.length_unit".lower()] = [
+            "CONTROLLEDVOCABULARY",
+            "length_unit",
+            "length_unit",
+            "LENGTH_VOCAB",
+        ]
+        output_dict[f"{sample_type_code}.weight_unit".lower()] = [
+            "CONTROLLEDVOCABULARY",
+            "weight_unit",
+            "weight_unit",
+            "WEIGHT_VOCAB",
+        ]
 
         return output_dict
 
 
-def _create_unit_vocabularies(o: Interbis, length_vocab_name: str = 'LENGTH_VOCAB',
-                              weight_vocab_name: str = 'WEIGHT_VOCAB'):
+def _create_unit_vocabularies(
+    o: ExtOpenbis,
+    length_vocab_name: str = "LENGTH_VOCAB",
+    weight_vocab_name: str = "WEIGHT_VOCAB",
+):
     length_terms = [
-        {"code": 'mm', "label": "mm", "description": "unit mm"},
-        {"code": 'cm', "label": "cm", "description": "unit cm"},
-        {"code": 'm', "label": "m", "description": "unit m"},
+        {"code": "mm", "label": "mm", "description": "unit mm"},
+        {"code": "cm", "label": "cm", "description": "unit cm"},
+        {"code": "m", "label": "m", "description": "unit m"},
     ]
     weight_terms = [
-        {"code": 'mg', "label": "mg", "description": "unit mg"},
-        {"code": 'g', "label": "g", "description": "unit g"},
-        {"code": 'kg', "label": "kg", "description": "unit kg"}
+        {"code": "mg", "label": "mg", "description": "unit mg"},
+        {"code": "g", "label": "g", "description": "unit g"},
+        {"code": "kg", "label": "kg", "description": "unit kg"},
     ]
 
     try:
@@ -182,7 +231,7 @@ def _create_unit_vocabularies(o: Interbis, length_vocab_name: str = 'LENGTH_VOCA
         _ = o.new_vocabulary(
             code=length_vocab_name,
             terms=length_terms,
-            description='Vocabulary for length units'
+            description="Vocabulary for length units",
         ).save()
 
     try:
@@ -193,20 +242,24 @@ def _create_unit_vocabularies(o: Interbis, length_vocab_name: str = 'LENGTH_VOCA
         _ = o.new_vocabulary(
             code=weight_vocab_name,
             terms=weight_terms,
-            description='Vocabulary for weight units'
+            description="Vocabulary for weight units",
         ).save()
 
 
-def _create_logfiles(mixture_sample_type: Union[SampleType, str], emodul_sample_type: Union[SampleType, str], ingredient_sample_type: Union[SampleType, str],
-                     logging_path: Union[Path, str]):
-    with open(Path(logging_path, 'mixture_sample_type.yaml'), 'w') as file:
+def _create_logfiles(
+    mixture_sample_type: Union[SampleType, str],
+    emodul_sample_type: Union[SampleType, str],
+    ingredient_sample_type: Union[SampleType, str],
+    logging_path: Union[Path, str],
+):
+    with open(Path(logging_path, "mixture_sample_type.yaml"), "w") as file:
         # file.write(yaml.dump({'Sample Type': mixture_sample_type}))
         print(mixture_sample_type, file=file)
 
-    with open(Path(logging_path, 'emodul_sample_type.yaml'), 'w') as file:
+    with open(Path(logging_path, "emodul_sample_type.yaml"), "w") as file:
         # file.write(yaml.dump({'Sample Type': emodul_sample_type}))
         print(emodul_sample_type, file=file)
 
-    with open(Path(logging_path, 'ingredient_sample_type.yaml'), 'w') as file:
+    with open(Path(logging_path, "ingredient_sample_type.yaml"), "w") as file:
         # file.write(yaml.dump({'Sample Type': emodul_sample_type}))
         print(ingredient_sample_type, file=file)
