0d583525ff61c4fae58b076897972ad9f67225cf
mattheokru <113978670+mattheokru@users.noreply.github.com>
2024-04-09 11:50:01 +0200

    added specific search to query-simple

--- web/static/js/querypage-simple.js
+++ web/static/js/querypage-simple.js
@@ -6,30 +6,59 @@ function downloadTable() {
     table.download("csv", "daten.csv");
 }
 
+
+// Cleans the data retrieved from Sparql Query (Removes URI and UUID4)
+function cleanEntry(entry) {
+    // Überprüfung auf UUID4 am Ende und Entfernen
+    const uuidRegex = /_[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+    if (uuidRegex.test(entry)) {
+        entry = entry.replace(uuidRegex, '');
+    }
+
+    // Überprüfung, ob der Eintrag mit einer URL beginnt und Entfernen bis zum letzten "/"
+    const urlRegex = /^(https?:\/\/[^\/]+\/.*)$/;
+    if (urlRegex.test(entry)) {
+        const lastSlashIndex = entry.lastIndexOf('/');
+        if (lastSlashIndex > -1) {
+            entry = entry.substring(lastSlashIndex + 1);
+        }
+    }
+
+    return entry;
+}
+
+
+// Creates a Query based on the specific input from the user
 function create_query(selectedQueryType, enteredName){
+    // Show all Mixtures with Name and ID
     if (selectedQueryType === "Mischung" && enteredName === "") {
         query = `
-        SELECT ?b WHERE {
+        SELECT ?ID ?Name WHERE {
           ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/cpto/MaterialComposition>.
           ?s <http://purl.org/spar/datacite/hasIdentifier> ?o.
+          ?s <http://purl.org/spar/datacite/hasIdentifier> ?p.
           ?o <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#NamedIndividual>.
-          ?o <https://w3id.org/pmd/co/value> ?b.
+          ?o <https://w3id.org/pmd/co/value> ?Name.
+          ?p <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#NamedIndividual>.
+          ?p <https://w3id.org/pmd/co/value> ?ID.
           FILTER(REGEX(str(?o), "/humanreadableID[^/]*$"))
+          FILTER(REGEX(str(?p), "/ID[^/]*$"))
         }`;
-        console.log("a")
     }
-    console.log("a")
+
+    // Show all Info from a specific Mixture
     if (selectedQueryType === "Mischung" && enteredName !== ""){
         query = `
-        SELECT ?a ?b WHERE {
+        SELECT ?Bestandteil ?Wert WHERE {
         ?g ?p "${enteredName}".
         BIND(SUBSTR(STR(?g), STRLEN(STR(?g)) - 35) AS ?suffix)
-        ?a <https://w3id.org/pmd/co/value> ?b.
-        FILTER(STRENDS(STR(?a), ?suffix))
+        ?Bestandteil <https://w3id.org/pmd/co/value> ?Wert.
+        FILTER(STRENDS(STR(?Bestandteil), ?suffix))
         }`;
-        console.log(query)
     }
 }
+
+// Generates the columns for Tabulator
 function generateColumns(vars) {
     return vars.map(varName => ({
         title: varName.toUpperCase(),  // Spaltentitel als Großbuchstaben der Variablennamen
@@ -39,17 +68,22 @@ function generateColumns(vars) {
     }));
 }
 
+// Transforms JSON response data from Backend for Tabulator
 function transformData(bindings, vars) {
     return bindings.map(binding => {
         let row = {};
         vars.forEach(varName => {
             // Extrahiert den Wert für jede Variable in jeder Bindung
-            row[varName] = binding[varName].value;
+            let rawValue = binding[varName].value;
+            // Reinigt den Wert vor der Zuweisung zum row-Objekt
+            let cleanedValue = cleanEntry(rawValue);
+            row[varName] = cleanedValue;
         });
         return row;
     });
 }
 
+// Send Sparql Query to backend and transform results
 function executeSparqlQuery() {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/queryexec', true);
@@ -83,7 +117,7 @@ function executeSparqlQuery() {
 }
 
 
-
+// Form event Listener
 document.getElementById('sparqlForm').addEventListener('submit', function(e) {
     e.preventDefault(); // Standard-Formular-Submit unterbrechen
     var queryTypeSelect = document.getElementById("queryType");
