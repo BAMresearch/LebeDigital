bce06fbe01835a5e2ee0c0ec63314b9e8d19aac8
LamiaIslam75 <57487898+LamiaIslam75@users.noreply.github.com>
2024-11-01 13:20:03 +0100

    ADD: script for compressive strength xml file to json

--- scripts/rawdataextraction/ComSt_xml_to_json.py
+++ scripts/rawdataextraction/ComSt_xml_to_json.py
@@ -1,11 +1,23 @@
-import os
 import xml.etree.ElementTree as ET
 import json
 import datetime
 import re
 import uuid
+import io
 
-def xml_to_json(xml_file, ComSt_json_file, specimen_json_file):
+def comSt_xml_to_json(xml_binary, mix_json):
+    """
+    Creates two json-files (E-module and Specimen) containing the
+    data from the xml file
+
+    :param xml_binary: xml BLOB from db
+    :param mix_json: Json from the Mixture used (binary format)
+
+    :return: [ComSt_json, specimen_json]
+    """
+
+    xml_file = io.BytesIO(xml_binary)
+
     # Parse the XML file
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -23,38 +35,17 @@ def xml_to_json(xml_file, ComSt_json_file, specimen_json_file):
     # set experiment lab location to BAM
     ComSt_data['Lab'] = 'BAM'
 
-    # path to XML file
-    xml_file_name = os.path.basename(xml_file_path)
-    # Extracting the name of the XML file without extension
-    xml_file_name_without_extension = os.path.splitext(xml_file_name)[0]
-    # Extracting the part before the last underscore
-    mix_json_file_name = xml_file_name_without_extension.rsplit('_', 2)[0] + ".json"
-
-    # Adjusting the path to the mix JSON file
-    mix_json_file_path = os.path.join("../../../usecases/MinimumWorkingExample/Druckfestigkeit/metadata_json_files/",
-                                      mix_json_file_name)
-
-    # save Mixdesign ID to specimen metadata
+    # Process mix_json data
     try:
-        with open(mix_json_file_path, "r", encoding="utf8", errors='ignore') as mixjson:
-            mixdesign = json.load(mixjson)
-            mixtureID = mixdesign['ID']
-            specimen_data['MixtureID'] = mixtureID
-            # Extract mixing date
-            mixing_date_str = mixdesign['MixingDate']
-            mixing_date = datetime.datetime.strptime(mixing_date_str, '%Y-%m-%dT%H:%M:%S')
-    except FileNotFoundError:
-        raise Exception("No mixdesign json-file found! Can't import the ID and save it to the output!")
-
-    # Get the directory of the XML file
-    xml_dir = os.path.dirname(xml_file)
-
-    # Create the path for the raw data file by replacing the extension
-    rawdata_file_name = os.path.splitext(os.path.basename(xml_file))[0] + ".xml"
-    rawdata_file_path = os.path.join(xml_dir, rawdata_file_name)
+        mixdesign = json.loads(mix_json.decode('utf-8'))
+        # Extract mixing date
+        mixing_date_str = mixdesign.get("MixingDate")
+        mixing_date = datetime.datetime.strptime(mixing_date_str, '%Y-%m-%dT%H:%M:%S')
+    except Exception as e:
+        raise Exception(f"Error processing mix_json: {str(e)}")
 
     # Add the raw data path to emodul_data
-    ComSt_data['RawDataFile'] = rawdata_file_path
+    ComSt_data['RawDataFile'] = "Download"
 
     # Iterate through each ArrayOfVariableData element
     has_diameter = False
@@ -68,7 +59,6 @@ def xml_to_json(xml_file, ComSt_json_file, specimen_json_file):
 
         # Map XML element names to JSON keys
             if name == 'TestRunDate':
-                ComSt_data['ExperimentDate'] = value
                 date_only = datetime.datetime.strptime(value.split(" ")[0], '%d.%m.%Y')
                 date_protegeformat = date_only.strftime('%Y-%m-%d') + "T" + value.split(" ")[1]
                 ComSt_data['ExperimentDate'] = str(date_protegeformat)
@@ -112,63 +102,18 @@ def xml_to_json(xml_file, ComSt_json_file, specimen_json_file):
         specimen_age = (experiment_date - mixing_date).days
         ComSt_data['SpecimenAge'] = specimen_age
         ComSt_data['SpecimenAge_Unit'] = 'day'
-    # Replace \u00b2, \u00b3, etc. with ^2, ^3, etc. in the JSON data
-    for key, value in ComSt_data.items():
-        if isinstance(value, str):
-            # Decode JSON string to handle Unicode escape sequences
-            decoded_value = json.loads(f'"{value}"')
 
-            # Replace \u00b2, \u00b3, etc. with ^2, ^3, etc.
-            decoded_value = re.sub(r'²', '^2', decoded_value)
-            decoded_value = re.sub(r'³', '^3', decoded_value)
+    # Replace special characters in both objects
+    for data in [ComSt_data, specimen_data]:
+        for key, value in data.items():
+            if isinstance(value, str):
+                # Decode JSON string to handle Unicode escape sequences
+                decoded_value = json.loads(f'"{value}"')
+                # Replace \u00b2, \u00b3, etc. with ^2, ^3, etc.
+                decoded_value = re.sub(r'²', '^2', decoded_value)
+                decoded_value = re.sub(r'³', '^3', decoded_value)
+                data[key] = decoded_value
 
-            ComSt_data[key] = decoded_value
-
-    # Replace \u00b2, \u00b3, etc. with ^2, ^3, etc. in the JSON data
-    for key, value in specimen_data.items():
-        if isinstance(value, str):
-            # Decode JSON string to handle Unicode escape sequences
-            decoded_value = json.loads(f'"{value}"')
-
-            # Replace \u00b2, \u00b3, etc. with ^2, ^3, etc.
-            decoded_value = re.sub(r'²', '^2', decoded_value)
-            decoded_value = re.sub(r'³', '^3', decoded_value)
-
-            specimen_data[key] = decoded_value
-
-    # Write emodul_data to JSON file
-    with open(ComSt_json_file, 'w', encoding='utf-8') as f:
-        json.dump(ComSt_data, f, indent=4)
-
-    with open(specimen_json_file, 'w', encoding='utf-8') as f:
-        json.dump(specimen_data, f, indent=4)
-
-#def main():
-    # Folder containing XML files
-#    xml_folder = "../../../usecases/MinimumWorkingExample/Data/E-Modul_28_Tage"
-
-    # Folder for JSON output files
-#    json_folder = "path/to/your/json_folder"
-
-    # Iterate through XML files in the folder
-#    for file_name in os.listdir(xml_folder):
-#        if file_name.endswith(".xml"):
-#            xml_file_path = os.path.join(xml_folder, file_name)
-#            json_file_path = os.path.join(json_folder, file_name.replace(".xml", ".json"))
-#            xml_to_json(xml_file_path, json_file_path)
-#            print("Conversion successful. JSON file saved at:", json_file_path)
-
-#if __name__ == "__main__":
-#    main()
-
-
-# Example usage:
-xml_file_path = "../../../usecases/MinimumWorkingExample/Data/Druckfestigkeit_BAM/20240305_7188_M05_Z04_DF.xml"
-emodul_json_file_path = "../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/20240305_7188_M05_Z04_DF.json"
-specimen_json_file_path = "../../../usecases/MinimumWorkingExample/emodul/metadata_json_files/220240305_7188_M05_Z04_DF_Specimen.json"
-
-xml_to_json(xml_file_path, emodul_json_file_path, specimen_json_file_path)
-
-print("Conversion successful. JSON files saved at:", emodul_json_file_path, specimen_json_file_path)
+    return [ComSt_data, specimen_data]
 
 
